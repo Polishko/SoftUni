73662fd294e913e1f651ca677ba984be3cc9656f
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-07-30 15:34:28 +0300

    Update horse_race_app.py
    Added tests

--- OOP-Exam-Questions/14 August 2022/project/horse_race_app.py
+++ OOP-Exam-Questions/14 August 2022/project/horse_race_app.py
@@ -112,3 +112,62 @@ class HorseRaceApp:
         return f"The winner of the {race_type} race," \
                f" with a speed of {winner.horse.speed}km/h" \
                f" is {winner.name}! Winner's horse: {winner.horse.name}."
+
+
+        
+# tests (mine)
+# from project.jockey import Jockey
+# from project.horse_specification.appaloosa import Appaloosa
+# from project.horse_specification.thoroughbred import Thoroughbred
+# from project.horse_race_app import HorseRaceApp
+# from project.horse_race import HorseRace
+# 
+# # jockey_1 = Jockey("Nalan", 43)
+# # print(jockey_1.name)
+# # print(jockey_1.age)
+# # print(jockey_1.horse)
+# # horse_1 = Appaloosa("Beyaz Yildirim", 118)
+# # horse_2 = Thoroughbred("Kara Bela", 137)
+# # print(horse_1.speed)
+# # print(horse_2.speed)
+# # horse_1.train()
+# # horse_2.train()
+# # print(horse_1.speed)
+# # print(horse_2.speed)
+# # horse_1.train()
+# # horse_2.train()
+# # print(horse_1.speed)
+# # print(horse_2.speed)
+# # app = HorseRaceApp()
+# # print(app.add_horse("Appaloosa", "Beyaz Yildirim", 118))
+# # print(app.add_horse("Thoroughbred", "Kara Bela", 137))
+# # print(app.add_horse("Appaloosa", "Krema", 120))
+# # print(len(app.horses))
+# # print(app.add_jockey("Nalan", 43))
+# # print(app.add_jockey("Genc", 42))
+# # print(len(app.jockeys))
+# # print(app.create_horse_race("Winter"))
+# # print(app.create_horse_race("Spring"))
+# # print(app.create_horse_race("Autumn"))
+# # print(app.create_horse_race("Summer"))
+# # print(len(app.horse_races))
+# # print(app.add_horse_to_jockey("Nalan", "Appaloosa"))
+# # print(app.jockeys[0].horse.is_taken)
+# # print(app.add_horse_to_jockey("Nalan", "Thoroughbred"))
+# # # print(app.add_horse_to_jockey("Leman", "Thoroughbred"))
+# 
+# # given tests
+# 
+# horseRaceApp = HorseRaceApp()
+# print(horseRaceApp.add_horse("Appaloosa", "Spirit", 80))
+# print(horseRaceApp.add_horse("Thoroughbred", "Rocket", 110))
+# print(horseRaceApp.add_jockey("Peter", 19))
+# print(horseRaceApp.add_jockey("Mariya", 21))
+# print(horseRaceApp.create_horse_race("Summer"))
+# print(horseRaceApp.add_horse_to_jockey("Peter", "Appaloosa"))
+# print(horseRaceApp.add_horse_to_jockey("Peter", "Thoroughbred"))
+# print(horseRaceApp.add_horse_to_jockey("Mariya", "Thoroughbred"))
+# print(horseRaceApp.add_jockey_to_horse_race("Summer", "Mariya"))
+# print(horseRaceApp.add_jockey_to_horse_race("Summer", "Peter"))
+# print(horseRaceApp.add_jockey_to_horse_race("Summer", "Mariya"))
+# print(horseRaceApp.start_horse_race("Summer"))
