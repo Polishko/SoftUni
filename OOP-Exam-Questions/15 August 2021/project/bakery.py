5939a17e8a244195e702d8660a45962061e4531f
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-08-01 11:47:26 +0300

    Update bakery.py
    Added testing options

--- OOP-Exam-Questions/15 August 2021/project/bakery.py
+++ OOP-Exam-Questions/15 August 2021/project/bakery.py
@@ -141,3 +141,80 @@ class Bakery:
 
     def get_total_income(self):
         return f"Total income: {self.total_income:.2f}lv"
+
+# testing options_adapted from @Devihem's tests:
+# from project.bakery import Bakery
+
+# bakery = Bakery("Sunny")
+
+# print(f"{'# ADD_FOOD'}")
+# print(bakery.add_food("Bread", "Sour", 1.50))
+# print(bakery.add_food("Bread", "White", 1.30))
+# print(bakery.add_food("Cake", "Chocolate", 2.90))
+# print(bakery.add_food("Cake", "Vanilla", 2.90))
+# [print(repr(f)) for f in bakery.food_menu]
+
+# print(f"\n\n{'# ADD_DRINK'}")
+# print(bakery.add_drink("Water", "Spring", 250, "Bankya"))
+# print(bakery.add_drink("Water", "Mineral", 500, "Devin"))
+# print(bakery.add_drink("Tea", "Black", 250, "Ahmad Tea"))
+# print(bakery.add_drink("Tea", "Green", 250, "Lipton"))
+# [print(repr(d)) for d in bakery.drinks_menu]
+
+# print(f"\n\n{'# ADD_TABLE'}")
+# print(bakery.add_table("InsideTable", 5, 5))
+# print(bakery.add_table("OutsideTable", 52, 10))
+# print(bakery.add_table("OutsideTable", 95, 15))
+# print(bakery.add_table("InsideTable", 15, 15))
+# print(bakery.add_table("InsideTable", 4, 15))
+# [print(t.free_table_info()) for t in bakery.tables_repository]
+
+# print(f"\n\n{'# RESERVE_TABLE'}")
+# print(bakery.reserve_table(12))
+# [print(t.is_reserved) for t in bakery.tables_repository]
+# [print(t.number_of_people) for t in bakery.tables_repository]
+# print(bakery.reserve_table(20))
+
+# print(f"\n\n{'# ORDERS_FOOD'}")
+# print(bakery.order_food(95, "Sour", "White", "Chocolate", "Vanilla", "Strawberry", "Hazelnut", "Wholegrain"))
+# print(bakery.order_food(34, "Sour", "White", "Chocolate", "Vanilla", "Strawberry", "Hazelnut", "Wholegrain"))
+# #
+# print(f"\n\n{'# ORDERS_DRINK'}")
+# print(bakery.order_drink(95, "Spring", "Mineral", "Black", "Green", "White", "Tap", "Fruit"))
+# for t in bakery.tables_repository:
+#     if t.is_reserved:
+#         [print(repr(f)) for f in t.food_orders]
+# print()
+# for t in bakery.tables_repository:
+#     if t.is_reserved:
+#         [print(repr(d)) for d in t.drink_orders]
+
+
+# print(f"\n\n{'# LEAVE_TABLE'}")
+# print(bakery.leave_table(95))
+# [print(t.free_table_info()) for t in bakery.tables_repository]
+# [print(t.is_reserved) for t in bakery.tables_repository]
+# [print(t.number_of_people) for t in bakery.tables_repository]
+
+# #
+# print(f"\n\n{'# GET_TOTAL_INCOME'}")
+# print(bakery.get_total_income())
+
+# print(bakery.reserve_table(8))
+# print(f"\n\n{'# ORDERS_FOOD'}")
+# print(bakery.order_food(52, "Sour", "White", "Chocolate", "Vanilla", "Strawberry", "Hazelnut", "Wholegrain"))
+
+# print(f"\n\n{'# ORDERS_DRINK'}")
+# print(bakery.order_drink(52, "Spring", "Mineral", "Black", "Green", "White", "Tap", "Fruit"))
+
+# print(f"\n\n{'# LEAVE_TABLE'}")
+# print(bakery.leave_table(52))
+
+# print(f"\n\n{'# GET_TOTAL_INCOME'}")
+# print(bakery.get_total_income())
+
+# print(f"\n\n{'# RESERVE_TABLE'}")
+# print(bakery.reserve_table(4))
+
+# print(f"\n\n{'# TABLES_INFO'}")
+# print(bakery.get_free_tables_info())
