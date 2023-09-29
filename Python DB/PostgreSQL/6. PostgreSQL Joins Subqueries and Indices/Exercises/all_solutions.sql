4c4e348e7c808888ea9aace9114c7024e700a164
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-09-29 23:05:38 +0300

    Update all_solutions.sql
    Added some more solutions

--- Python DB/PostgreSQL/6. PostgreSQL Joins Subqueries and Indices/Exercises/all_solutions.sql
+++ Python DB/PostgreSQL/6. PostgreSQL Joins Subqueries and Indices/Exercises/all_solutions.sql
@@ -39,3 +39,64 @@ LIMIT
 ;
 
 -- 4.	Booking Information
+
+SELECT
+	b.booking_id AS "Booking ID",
+	a.name AS "Apartment Owner",
+	a.apartment_id AS "Apartment ID",
+	CONCAT(c.first_name, ' ', c.last_name) AS "Customer Name"
+FROM bookings AS b
+	FULL JOIN apartments AS a
+		ON b.booking_id = a.booking_id
+			FULL JOIN customers AS c
+				ON b.customer_id = c.customer_id
+ORDER BY
+	"Booking ID",
+	"Apartment Owner",
+	"Customer Name"
+;
+
+--5. Multiplication of Information**
+
+SELECT
+	b.booking_id AS "Booking ID",
+	c.first_name AS "Customer Name"
+FROM bookings AS b
+	CROSS JOIN customers AS c
+;
+
+--6. Unassigned Apartments
+
+SELECT
+	b.booking_id,
+	b.apartment_id,
+	c.companion_full_name
+FROM bookings AS b
+	JOIN customers AS c
+		ON b.customer_id = c.customer_id
+WHERE b.apartment_id IS NULL
+;
+
+--7. Bookings Made by Lead
+
+SELECT
+	b.apartment_id,
+	b.booked_for,
+	c.first_name,
+	c.country
+FROM bookings AS b
+	JOIN customers AS c
+		ON b.customer_id = c.customer_id
+WHERE c.job_type LIKE '%Lead%'
+;
+
+--8. Hahn's Bookings
+
+SELECT
+	COUNT(c.customer_id)
+FROM bookings AS b
+	JOIN customers AS c
+		ON b.customer_id = c.customer_id
+WHERE c.last_name = 'Hahn'
+;
+
