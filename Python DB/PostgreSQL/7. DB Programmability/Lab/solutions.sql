50f3d8fa739c6ec0e80997680cc6e5cbda620f3a
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-10-04 22:05:24 +0300

    Update solutions.sql
    Added solution

--- Python DB/PostgreSQL/7. DB Programmability/Lab/solutions.sql
+++ Python DB/PostgreSQL/7. DB Programmability/Lab/solutions.sql
@@ -39,7 +39,77 @@ AS $$
     END;
 $$ LANGUAGE plpgsql;
 
+-- OR
+
+CREATE OR REPLACE FUNCTION fn_count_employees_by_town(town_name VARCHAR(20))
+RETURNS INT
+AS $$
+    DECLARE
+    BEGIN
+        RETURN
+        (SELECT
+	        COUNT(e.employee_id)
+        FROM towns AS t
+	        JOIN addresses AS a
+		        ON t.town_id = a.town_id
+			        JOIN employees AS e
+				        ON a.address_id = e.address_id
+        WHERE t."name" = town_name);
+    END;
+$$ LANGUAGE plpgsql;
+
+
 -- test:
 SELECT fn_count_employees_by_town(NULL)
 
 --2.	Employees Promotion
+
+CREATE PROCEDURE sp_increase_salaries(department_name VARCHAR(50))
+AS
+$$
+    BEGIN
+            UPDATE employees AS e
+            SET salary = salary * 1.05
+            WHERE e.department_id = (
+                SELECT
+                    d.department_id
+                FROM
+                    departments as d
+                WHERE
+                    d.name = department_name
+                );
+    END;
+$$ LANGUAGE plpgsql;
+
+--OR
+
+CREATE PROCEDURE sp_increase_salaries(department_name VARCHAR(50))
+AS
+$$
+    BEGIN
+            UPDATE employees AS e
+            SET salary = salary * 1.05
+            WHERE e.department_id = (
+                SELECT
+                    d.department_id
+                FROM
+                    departments as d
+                JOIN
+                    employees AS e
+                ON
+                    d.department_id = e.department_id
+                WHERE
+                    d.name = department_name
+                GROUP BY
+                    d.department_id
+                );
+    END;
+$$ LANGUAGE plpgsql;
+
+
+
+--test
+
+CALL sp_increase_salaries('Finance');
+
+
