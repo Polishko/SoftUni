b9ace347a5081e775dc8ce21b323e9c2cff0ea0b
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-10-05 19:27:45 +0300

    Update solutions.sql
    Added some solutions

--- Python DB/PostgreSQL/7. DB Programmability/Exercise/solutions.sql
+++ Python DB/PostgreSQL/7. DB Programmability/Exercise/solutions.sql
@@ -1,1 +1,193 @@
---1
+--1.	User-defined Function Full Name
+
+CREATE OR REPLACE FUNCTION fn_full_name(first_name VARCHAR(50), last_name VARCHAR(50))
+RETURNS VARCHAR(100)
+AS
+$$
+    DECLARE
+        full_name VARCHAR(100);
+    BEGIN
+        IF first_name IS NULL AND last_name IS NULL THEN
+            full_name := NULL;
+        ELSIF first_name IS NULL THEN
+            full_name := initcap(last_name);
+        ELSIF last_name IS NULL THEN
+            full_name := initcap(first_name);
+        ELSE
+            full_name := concat(initcap(first_name), ' ', initcap(last_name));
+        END IF;
+    RETURN full_name;
+    END
+$$
+LANGUAGE plpgsql;
+
+--2.	User-defined Function Future Value
+
+CREATE OR REPLACE FUNCTION fn_calculate_future_value(
+    initial_sum DECIMAL,
+    yearly_interest_rate DECIMAL,
+    number_of_years INT
+)
+RETURNS DECIMAL
+AS $$
+    DECLARE
+        future_value DECIMAL;
+    BEGIN
+        future_value := trunc(initial_sum * power(1 + yearly_interest_rate, number_of_years), 4);
+    RETURN future_value;
+    END;
+$$
+LANGUAGE plpgsql;
+
+--OR
+
+CREATE OR REPLACE FUNCTION fn_calculate_future_value(
+    IN initial_sum DECIMAL,
+    IN yearly_interest_rate DECIMAL,
+    IN number_of_years INT,
+    OUT future_value DECIMAL
+)
+
+AS $$
+    BEGIN
+        future_value := trunc(initial_sum * power(1 + yearly_interest_rate, number_of_years), 4);
+    END;
+$$
+LANGUAGE plpgsql;
+
+--exammple
+SELECT fn_calculate_future_value (1000, 0.1, 5);
+
+--3.	User-defined Function Is Word Comprised
+
+CREATE OR REPLACE FUNCTION fn_is_word_comprised(
+    IN set_of_letters VARCHAR(50),
+    IN word VARCHAR(50),
+    OUT bool_val BOOLEAN
+)
+AS $$
+    DECLARE
+        sub_letter CHAR(1);
+        pos_letter INT;
+        sub_set VARCHAR(50);
+    BEGIN
+        sub_set := LOWER(set_of_letters);
+
+        WHILE CHAR_LENGTH(word) > 0 LOOP
+            sub_letter := LEFT(LOWER(word), 1);
+            pos_letter := POSITION(sub_letter IN sub_set);
+            IF pos_letter = 0 THEN
+                bool_val := FALSE;
+                RETURN;
+            ELSE
+                word := SUBSTRING(word FROM 2);
+                sub_set := LEFT(sub_set, pos_letter - 1) || RIGHT(sub_set, CHAR_LENGTH(sub_set) - pos_letter);
+            END IF;
+        END LOOP;
+        bool_val := TRUE;
+
+    END;
+$$
+LANGUAGE plpgsql;
+
+--4.	Game Over
+
+CREATE OR REPLACE FUNCTION fn_is_game_over(is_game_over BOOLEAN)
+RETURNS TABLE(name VARCHAR(50), game_type_id INT, is_finished BOOLEAN)
+AS $$
+    BEGIN
+        RETURN QUERY
+        SELECT
+	        g.name,
+	        g.game_type_id,
+	        g.is_finished
+        FROM games AS g
+        WHERE g.is_finished = is_game_over;
+    END;
+$$
+LANGUAGE plpgsql;
+
+--5. Difficulty Level
+
+CREATE OR REPLACE FUNCTION fn_difficulty_level(
+    IN level INT,
+    OUT difficulty_level VARCHAR(50)
+)
+AS $$
+    BEGIN
+        IF level <= 40 THEN
+           difficulty_level := 'Normal Difficulty';
+        ELSIF level >= 41 AND level <= 60 THEN
+            difficulty_level := 'Nightmare Difficulty';
+        ELSE
+            difficulty_level := 'Hell Difficulty';
+        END IF;
+    END
+$$
+LANGUAGE plpgsql;
+
+--6. * Cash in User Games Odd Rows
+
+CREATE OR REPLACE FUNCTION fn_cash_in_users_games(game_name VARCHAR(50))
+RETURNS TABLE(total_cash NUMERIC)
+AS $$
+    BEGIN
+        RETURN QUERY
+        SELECT ROUND(SUM(cash)::NUMERIC, 2) AS "total_cash"
+    FROM (
+        SELECT cash,
+               ROW_NUMBER() OVER (ORDER BY cash DESC) AS "row_number"
+        FROM users_games AS ug
+        JOIN games AS g
+			ON ug.game_id = g.id
+        WHERE g.name = game_name
+    ) AS joined_table
+    WHERE joined_table.row_number % 2 != 0;
+    END
+$$
+LANGUAGE plpgsql;
+
+--7. Retrieving Account Holders**
+
+CREATE OR REPLACE PROCEDURE sp_retrieving_holders_with_balance_higher_than(searched_balance NUMERIC)
+AS $$
+    DECLARE
+        row_data RECORD;
+
+        my_cursor CURSOR FOR
+            SELECT
+            ah.id,
+            ah.first_name,
+            ah.last_name,
+            SUM(a.balance) AS "total_balance"
+        FROM accounts AS a
+            JOIN account_holders AS ah
+                ON a.account_holder_id = ah.id
+        GROUP BY
+            ah.id,
+            ah.first_name,
+            ah.last_name
+        ORDER BY
+            ah.first_name,
+            ah.last_name;
+
+    BEGIN
+
+        OPEN my_cursor;
+        LOOP
+            FETCH my_cursor INTO row_data;
+            EXIT WHEN NOT FOUND;
+            IF row_data.total_balance > searched_balance THEN
+                RAISE NOTICE '% % - %', row_data.first_name, row_data.last_name, row_data.total_balance;
+            END IF;
+        END LOOP;
+
+        CLOSE my_cursor;
+
+    END;
+$$
+LANGUAGE plpgsql;
+
+-- test
+CALL sp_retrieving_holders_with_balance_higher_than(200000);
+
