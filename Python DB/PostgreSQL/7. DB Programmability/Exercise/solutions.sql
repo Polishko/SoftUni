f1e51a27af6f880dfc7e8691fbd457e4f202248f
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-10-05 21:58:48 +0300

    Update solutions.sql
    last question left

--- Python DB/PostgreSQL/7. DB Programmability/Exercise/solutions.sql
+++ Python DB/PostgreSQL/7. DB Programmability/Exercise/solutions.sql
@@ -265,5 +265,36 @@ AS $$
     END;
 $$;
 
+--11. Delete Procedure
+
+DROP PROCEDURE sp_retrieving_holders_with_balance_higher_than;
+
+--12. Log Accounts Trigger
+
+CREATE TABLE logs(
+    id SERIAL PRIMARY KEY,
+    account_id INT,
+    old_sum NUMERIC,
+    new_sum NUMERIC
+);
+
+CREATE OR REPLACE FUNCTION trigger_fn_insert_new_entry_into_logs()
+    RETURNS TRIGGER
+AS $$
+    BEGIN
+        INSERT INTO logs(account_id, old_sum, new_sum)
+        VALUES (OLD.id, OLD.balance, NEW.balance);
+    RETURN NEW;
+    END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER tr_account_balance_change
+    AFTER UPDATE OF balance ON accounts
+    FOR EACH ROW
+    WHEN (OLD.balance IS DISTINCT FROM NEW.balance)
+    EXECUTE FUNCTION trigger_fn_insert_new_entry_into_logs();
+
+--13. Notification Email on Balance Change
 
 
