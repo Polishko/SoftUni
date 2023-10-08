3ce4b34e0d68b876de482b56633e0d99964a6f28
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-10-08 20:20:04 +0300

    Update solutions.sql
    Added few solutions

--- Python DB/PostgreSQL/Exam Preparation 3/solutions.sql
+++ Python DB/PostgreSQL/Exam Preparation 3/solutions.sql
@@ -79,3 +79,47 @@ CREATE TABLE creators_board_games(
                 ON UPDATE CASCADE
                 ON DELETE CASCADE
 );
+
+--2. Insert
+
+INSERT INTO board_games(name, release_year, rating,	category_id, publisher_id, players_range_id)
+    VALUES
+        ('Deep Blue', '2019', 5.67,	1, 15, 7),
+        ('Paris', '2016', 9.78, 7, 1, 5),
+        ('Catan: Starfarers', '2021',	9.87, 7, 13, 6),
+        ('Bleeding Kansas',	'2020',	3.25, 3, 7,	4),
+        ('One Small Step', '2019', 5.75, 5,	9, 2)
+;
+
+INSERT INTO publishers(name, address_id, website, phone)
+    VALUES
+        ('Agman Games', 5, 'www.agmangames.com', '+16546135542'),
+        ('Amethyst Games', 7, 'www.amethystgames.com', '+15558889992'),
+        ('BattleBooks',	13,	'www.battlebooks.com', '+12345678907')
+;
+
+--3.	Update
+
+UPDATE players_ranges
+SET  max_players = max_players + 1
+WHERE max_players <= 2 AND min_players >= 2;
+
+UPDATE board_games
+SET name = CONCAT(name, ' V2')
+WHERE release_year >= 2020;
+
+--4.	Delete
+
+ALTER TABLE board_games
+DROP CONSTRAINT fk_board_games_publishers;
+
+ALTER TABLE board_games
+ADD CONSTRAINT fk_board_games_addresses
+FOREIGN KEY(publisher_id)
+REFERENCES publishers(id)
+ON DELETE CASCADE ;
+
+DELETE FROM addresses CASCADE
+WHERE SUBSTRING(town FROM 1 FOR 1) = 'L';
+
+--5.	Board Games by Release Year
