f0abb80fe9694b3d408074a08a44e9c43a913ba5
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-10-09 12:45:52 +0300

    Update solutions.sql
    added final questions

--- Python DB/PostgreSQL/Exam Preparation 3/solutions.sql
+++ Python DB/PostgreSQL/Exam Preparation 3/solutions.sql
@@ -123,3 +123,181 @@ DELETE FROM addresses CASCADE
 WHERE SUBSTRING(town FROM 1 FOR 1) = 'L';
 
 --5.	Board Games by Release Year
+
+SELECT
+    name,
+    rating
+FROM board_games
+ORDER BY release_year, name DESC;
+
+--6.	Board Games by Category
+
+SELECT
+    bg.id,
+    bg.name,
+    bg.release_year,
+    c.name
+FROM board_games AS bg
+    JOIN categories AS c
+        ON bg.category_id = c.id
+WHERE
+    c.name = 'Strategy Games' OR c.name = 'Wargames'
+ORDER BY
+    bg.release_year DESC
+;
+
+--7.	Creators without Board Games
+
+SELECT
+    cr.id,
+    CONCAT(cr.first_name, ' ', cr.last_name) AS "creator_name",
+    cr.email
+FROM creators AS cr
+    LEFT JOIN creators_board_games AS cbg
+        ON cr.id = cbg.creator_id
+            LEFT JOIN board_games AS bg
+                ON cbg.board_game_id = bg.id
+WHERE
+    bg.name IS NULL
+ORDER BY
+    creator_name
+;
+
+--8.	First 5 Board Games
+
+SELECT
+    bg.name,
+    bg.rating,
+    ca.name
+FROM board_games AS bg
+    JOIN players_ranges AS pr
+        ON bg.players_range_id = pr.id
+            JOIN categories AS ca
+                ON bg.category_id = ca.id
+WHERE
+    (bg.rating > 7.00 AND (LOWER(bg.name) LIKE '%a%' OR bg.rating > 7.50)) AND (pr.min_players >= 2 AND pr.max_players <= 5)
+ORDER BY
+    bg.name,
+    bg.rating DESC
+LIMIT
+    5
+;
+
+--9.	Creators with Emails
+
+SELECT
+    CONCAT(cr.first_name, ' ', cr.last_name) AS "full_name",
+    cr.email,
+    MAX(b.rating)
+FROM creators_board_games AS crb
+    JOIN creators AS cr
+        ON crb.creator_id = cr.id
+            JOIN board_games AS b
+                ON crb.board_game_id = b.id
+WHERE
+    cr.email LIKE '%.com'
+GROUP BY
+    full_name,
+    cr.email
+ORDER BY
+    full_name
+;
+
+--10.	Creators by Rating
+
+SELECT
+    cr.last_name,
+    CEIL(AVG(b.rating)) AS "average_rating",
+    p.name
+FROM board_games AS b
+    JOIN creators_board_games AS crb
+        ON b.id = crb.board_game_id
+            JOIN creators AS cr
+                ON crb.creator_id = cr.id
+                    JOIN publishers AS p
+                        ON b.publisher_id = p.id
+WHERE
+    p.name = 'Stonemaier Games'
+GROUP BY
+    cr.last_name,
+    cr.email,
+    p.name
+ORDER BY
+    average_rating DESC
+;
+
+--11.	Creator of Board Games
+
+CREATE OR REPLACE FUNCTION fn_creator_with_board_games(
+    IN input_first_name VARCHAR(30),
+    OUT total_number_of_board_games INT)
+AS $$
+    BEGIN
+
+        SELECT INTO total_number_of_board_games
+            COUNT(cbg.board_game_id)
+        FROM creators AS c
+            LEFT JOIN creators_board_games AS cbg
+                ON c.id = cbg.creator_id
+        WHERE
+            c.first_name = input_first_name
+        ;
+    END
+$$
+LANGUAGE plpgsql;
+
+--12.	Search for Board Games
+
+CREATE TABLE IF NOT EXISTS search_results (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    release_year INT,
+    rating FLOAT,
+    category_name VARCHAR(50),
+    publisher_name VARCHAR(50),
+    min_players VARCHAR(50),
+    max_players VARCHAR(50)
+);
+
+CREATE OR REPLACE PROCEDURE usp_search_by_category(category varchar(50))
+AS $$
+    DECLARE
+    BEGIN
+        TRUNCATE TABLE search_results;
+        INSERT INTO search_results(
+            name,
+            release_year,
+            rating,
+            category_name,
+            publisher_name,
+            min_players,
+            max_players)
+        SELECT
+            bg.name,
+            bg.release_year,
+            bg.rating,
+            c.name,
+            p.name,
+            CONCAT(pl.min_players, ' people'),
+            CONCAT(pl.max_players, ' people')
+        FROM board_games AS bg
+            JOIN categories AS c
+                ON bg.category_id = c.id
+                    JOIN publishers AS p
+                        ON bg.publisher_id = p.id
+                            JOIN players_ranges AS pl
+                                ON bg.players_range_id = pl.id
+        WHERE
+            c.name = category
+        ORDER BY
+            p.name,
+            bg.release_year DESC
+        ;
+    END
+$$
+LANGUAGE plpgsql;
+
+--TEST
+CALL usp_search_by_category('Wargames');
+
+SELECT * FROM search_results;
