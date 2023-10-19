0e66ed7d79fb7673d967455e0ec8209a6e08f525
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-10-19 12:07:13 +0300

    Update models.py
    removed comment selections

--- Python - ORM/01. Django Model Basics/Exercise/orm_skeleton_lab_02/main_app/models.py
+++ Python - ORM/01. Django Model Basics/Exercise/orm_skeleton_lab_02/main_app/models.py
@@ -8,59 +8,59 @@ GENRES = [
 ]
 
 
-# class Person(models.Model):
-#     name = models.CharField(
-#         max_length=30
-#     )
-#     age = models.PositiveIntegerField()
+class Person(models.Model):
+    name = models.CharField(
+        max_length=30
+    )
+    age = models.PositiveIntegerField()
 
 
-# class Blog(models.Model):
-#     post = models.TextField()
-#     author = models.CharField(max_length=35)
+class Blog(models.Model):
+    post = models.TextField()
+    author = models.CharField(max_length=35)
 
 
-# class WeatherForecast(models.Model):
-#     date = models.DateField()
-#     temperature = models.FloatField()
-#     humidity = models.FloatField()
-#     precipitation = models.FloatField()
+class WeatherForecast(models.Model):
+    date = models.DateField()
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    precipitation = models.FloatField()
 
 
-# class Recipe(models.Model):
-#     name = models.CharField(max_length=100, unique=True)
-#     description = models.TextField()
-#     ingredients = models.TextField()
-#     cook_time = models.PositiveIntegerField()
-#     created_at = models.DateTimeField(auto_now_add=True)
+class Recipe(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    ingredients = models.TextField()
+    cook_time = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
-# class Product(models.Model):
-#     name = models.CharField(max_length=70)
-#     description = models.TextField()
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-#     created_at = models.DateTimeField(auto_now_add=True)
+class Product(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
-# class UserProfile(models.Model):
-#     username = models.CharField(max_length=65, unique=True)
-#     first_name = models.CharField(max_length=40, unique=True)
-#     last_name = models.CharField(max_length=40, unique=True)
-#     email = models.EmailField(unique=True, default="students@softuni.bg")
-#     bio = models.TextField(max_length=120)
-#     profile_image_url = models.URLField()
-#     created_at = models.DateTimeField(auto_now_add=True)
+class UserProfile(models.Model):
+    username = models.CharField(max_length=65, unique=True)
+    first_name = models.CharField(max_length=40, unique=True)
+    last_name = models.CharField(max_length=40, unique=True)
+    email = models.EmailField(unique=True, default="students@softuni.bg")
+    bio = models.TextField(max_length=120)
+    profile_image_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
-# class Exercise(models.Model):
-#     name = models.CharField(max_length=100)
-#     description = models.TextField()
-#     difficulty_level = models.CharField(max_length=20)
-#     duration_minutes = models.PositiveIntegerField()
-#     equipment = models.CharField(max_length=90)
-#     video_url = models.URLField(null=True, blank=True)
-#     calories_burned = models.PositiveIntegerField(default=0)
-#     is_favorite = models.BooleanField(default=False)
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    difficulty_level = models.CharField(max_length=20)
+    duration_minutes = models.PositiveIntegerField()
+    equipment = models.CharField(max_length=90)
+    video_url = models.URLField(null=True, blank=True)
+    calories_burned = models.PositiveIntegerField(default=0)
+    is_favorite = models.BooleanField(default=False)
 
 
 class Book(models.Model):
