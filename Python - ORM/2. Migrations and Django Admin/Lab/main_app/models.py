dc8b70d58fc48826ce183d8919d8d7b749415041
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-10-20 07:56:27 +0300

    Added solution
    Added solution

---
+++ Python - ORM/2. Migrations and Django Admin/Lab/main_app/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=500
+    )
+    description = models.TextField(
+        null=True,
+        blank=True
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    category = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True)
+    supplier = models.CharField(
+        max_length=150, null=True,
+        blank=True
+    )
+    created_on = models.DateTimeField(
+        auto_now_add=True
+    )
+    last_edited_on = models.DateTimeField(
+        auto_now=True
+    )
+
+    barcode = models.IntegerField()
+
