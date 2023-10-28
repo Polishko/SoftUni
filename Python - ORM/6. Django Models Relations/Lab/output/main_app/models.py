dc275a00b656b90d9f9c7da18d5e3e0a1f386bf7
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-10-28 12:00:40 +0300

    Added solutions
    Added solutions

---
+++ Python - ORM/6. Django Models Relations/Lab/output/main_app/models.py
@@ -0,0 +1,108 @@
+from django.utils import timezone
+
+from django.db import models
+
+
+# Create your models here
+class Lecturer(models.Model):
+    first_name = models.CharField(
+        max_length=100
+    )
+    last_name = models.CharField(
+        max_length=100
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+# class Subject(models.Model):
+#     name = models.CharField(
+#         max_length=100
+#     )
+#     code = models.CharField(
+#         max_length=10
+#     )
+#     lecturer = models.ForeignKey(Lecturer, on_delete=models.SET_NULL, null=True)
+#
+#     def __str__(self):
+#         return f"The lecturer for {self.name} is {self.lecturer}."
+
+
+class Subject(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+    code = models.CharField(
+        max_length=10
+    )
+    lecturer = models.ForeignKey(Lecturer, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Student(models.Model):
+    student_id = models.CharField(
+        max_length=10,
+        primary_key=True,
+    )
+    first_name = models.CharField(
+        max_length=100
+    )
+    last_name = models.CharField(
+        max_length=100
+    )
+    birth_date = models.DateField()
+    email = models.EmailField(
+        unique=True
+    )
+    subjects = models.ManyToManyField(
+        Subject, through="StudentEnrollment")
+
+
+class StudentEnrollment(models.Model):
+    GRADES = [
+        ("A", "A"),
+        ("B", "B"),
+        ("C", "C"),
+        ("D", "D"),
+        ("F", "F")
+    ]
+
+    student = models.ForeignKey(
+        Student,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    subject = models.ForeignKey(
+        Subject,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    enrollment_date = models.DateField(
+        default=timezone.now
+    )
+    grade = models.CharField(
+        max_length=1,
+        choices=GRADES
+    )
+
+
+class LecturerProfile(models.Model):
+    lecturer = models.OneToOneField(
+        Lecturer,
+        on_delete=models.CASCADE
+    )
+    email = models.EmailField(
+        unique=True
+    )
+    bio = models.TextField(
+        null=True,
+        blank=True
+    )
+    office_location = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
