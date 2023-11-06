5977ae4b46e8f041f42e12a2902333d5e63fc890
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-11-06 11:07:23 +0200

    Added solutions
    Added solutions

---
+++ Python - ORM/7. Models Inheritance and Customization/Exercise/output/caller.py
@@ -0,0 +1,153 @@
+import os
+from datetime import date
+
+import django
+from django.core.exceptions import ValidationError
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+from main_app.models import (BaseCharacter, Mage, Assassin, DemonHunter,
+                             TimeMage, Necromancer, ViperAssassin, ShadowbladeAssassin,
+                             VengeanceDemonHunter, FelbladeDemonHunter,
+                             UserProfile, Message,
+                             Student, StudentIDField,
+                             CreditCard, MaskedCreditCardField,
+                             Hotel, Room, SpecialReservation, RegularReservation, BaseReservation)
+
+
+# Create queries within functions
+# 1.	Character Classes
+# Create instances
+# mage = Mage.objects.create(
+#     name="Fire Mage",
+#     description="A powerful mage specializing in fire magic.",
+#     elemental_power="Fire",
+#     spellbook_type="Ancient Grimoire"
+# )
+#
+# necromancer = Necromancer.objects.create(
+#     name="Dark Necromancer",
+#     description="A mage specializing in dark necromancy.",
+#     elemental_power="Darkness", spellbook_type="Necronomicon",
+#     raise_dead_ability="Raise Undead Army"
+# )
+# print(mage.elemental_power)
+# print(mage.spellbook_type)
+# print(necromancer.name)
+# print(necromancer.description)
+# print(necromancer.raise_dead_ability)
+
+
+# 2.	Chat App
+# Create users
+# user1 = UserProfile.objects.create(username='john_doe', email='john@example.com', bio='Hello, I am John Doe.')
+#
+# user2 = UserProfile.objects.create(username='jane_smith', email='jane@example.com', bio='Hi there, I am Jane Smith.')
+#
+# user3 = UserProfile.objects.create(username='alice', email='alice@example.com', bio='Hello, I am Alice.')
+#
+# # Create a message from user1 to user2
+# message1 = Message.objects.create(
+#     sender=user1,
+#     receiver=user2,
+#     content="Hello, Jane! Could you please tell Alice that tomorrow we are going on vacation?")
+# print(message1.content)
+#
+# # Mark the message as read
+# message1.mark_as_read()
+# print(f"Is read: {message1.is_read}")
+#
+# # Create a reply from user2 to user1
+# reply_message = message1.reply_to_message(
+#     receiver=user1,
+#     reply_content="Hi John, sure! I will forward this message to her!")
+# print(reply_message.content)
+#
+# # Create a forwarded message from user2 to user3
+# forwarded_message = message1.forward_message(sender=user2, receiver=user3)
+# print(f"Forwarded message from {forwarded_message.sender.username} to {forwarded_message.receiver.username}")
+
+
+# 3.	Student Information
+# Test cases
+# student1 = Student(name="John", student_id=12345)
+# student1.save()
+#
+# student2 = Student(name="Alice", student_id=45.23)
+# student2.save()
+#
+# student3 = Student(name="Bob", student_id="789")
+# student3.save()
+#
+# # Retrieving student IDs from the database
+# retrieved_student1 = Student.objects.get(name="John")
+# retrieved_student2 = Student.objects.get(name="Alice")
+# retrieved_student3 = Student.objects.get(name="Bob")
+#
+# print(retrieved_student1.student_id)
+# print(retrieved_student2.student_id)
+# print(retrieved_student3.student_id)
+
+
+# 4. Credit Card Masking
+# Create CreditCard instances with card owner names and card numbers
+# credit_card1 = CreditCard.objects.create(card_owner="Krasimir", card_number="1234567890123450")
+# credit_card2 = CreditCard.objects.create(card_owner="Pesho", card_number="9876543210987654")
+# credit_card3 = CreditCard.objects.create(card_owner="Vankata", card_number="4567890123456789")
+#
+# # Save the instances to the database
+# credit_card1.save()
+# credit_card2.save()
+# credit_card3.save()
+#
+# # Retrieve the CreditCard instances from the database
+# credit_cards = CreditCard.objects.all()
+#
+# # Display the card owner names and masked card numbers
+# for credit_card in credit_cards:
+#     print(f"Card Owner: {credit_card.card_owner}")
+#     print(f"Card Number: {credit_card.card_number}")
+# credit_card4 = CreditCard.objects.create(card_owner="Minka", card_number=2345678901234*46)
+
+# 5. Hotel
+# Create a Hotel instance
+# hotel = Hotel.objects.create(name="Hotel ABC", address="123 Main St")
+#
+# # Create Room instances associated with the hotel
+# room1 = Room.objects.create(
+#     hotel=hotel,
+#     number="101",
+#     capacity=2,
+#     total_guests=1,
+#
+#     price_per_night=100.00
+# )
+#
+# # Create SpecialReservation instances
+# special_reservation1 = SpecialReservation(
+#     room=room1,
+#     start_date=date(2023, 1, 1),
+#     end_date=date(2023, 1, 5)
+# )
+#
+# print(special_reservation1.save())
+#
+# special_reservation2 = SpecialReservation(
+#     room=room1,
+#     start_date=date(2023, 1, 10),
+#     end_date=date(2023, 1, 12)
+# )
+#
+# print(special_reservation2.save())
+#
+# print(special_reservation1.calculate_total_cost())
+# print(special_reservation1.reservation_period())
+#
+# # Example of extending a SpecialReservation
+# try:
+#     special_reservation1.extend_reservation(5)
+# except ValidationError as e:
+#     print(e)
---
+++ Python - ORM/7. Models Inheritance and Customization/Exercise/output/main_app/models.py
@@ -0,0 +1,294 @@
+from datetime import timedelta
+
+from django.db import models
+from django.core.exceptions import ValidationError
+from abc import abstractmethod
+
+
+# Create your models here.
+class BaseCharacter(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+    description = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class Mage(BaseCharacter):
+    elemental_power = models.CharField(
+        max_length=100
+    )
+    spellbook_type = models.CharField(
+        max_length=100
+    )
+
+
+class Assassin(BaseCharacter):
+    weapon_type = models.CharField(
+        max_length=100
+    )
+    assassination_technique = models.CharField(
+        max_length=100
+    )
+
+
+class DemonHunter(BaseCharacter):
+    weapon_type = models.CharField(
+        max_length=100
+    )
+    demon_slaying_ability = models.CharField(
+        max_length=100
+    )
+
+
+class TimeMage(Mage):
+    time_magic_mastery = models.CharField(
+        max_length=100
+    )
+    temporal_shift_ability = models.CharField(
+        max_length=100
+    )
+
+
+class Necromancer(Mage):
+    raise_dead_ability = models.CharField(
+        max_length=100
+    )
+
+
+class ViperAssassin(Assassin):
+    venomous_strikes_mastery = models.CharField(
+        max_length=100
+    )
+    venomous_bite_ability = models.CharField(
+        max_length=100
+    )
+
+
+class ShadowbladeAssassin(Assassin):
+    shadowstep_ability = models.CharField(
+        max_length=100
+    )
+
+
+class VengeanceDemonHunter(DemonHunter):
+    vengeance_mastery = models.CharField(
+        max_length=100
+    )
+    retribution_ability = models.CharField(
+        max_length=100
+    )
+
+
+class FelbladeDemonHunter(DemonHunter):
+    felblade_ability = models.CharField(
+        max_length=100
+    )
+
+
+class UserProfile(models.Model):
+    username = models.CharField(
+        max_length=70,
+        unique=True
+    )
+    email = models.EmailField(
+        unique=True
+    )
+    bio = models.TextField(
+        blank=True,
+        null=True
+    )
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        to=UserProfile,
+        related_name="sent_messages",
+        on_delete=models.CASCADE
+    )
+    receiver = models.ForeignKey(
+        to=UserProfile,
+        related_name="received_messages",
+        on_delete=models.CASCADE
+    )
+    content = models.TextField()
+    timestamp = models.DateTimeField(
+        auto_now_add=True
+    )
+    is_read = models.BooleanField(
+        default=False
+    )
+
+    def mark_as_read(self):
+        self.is_read = True
+
+    def mark_as_unread(self):
+        self.is_read = False
+
+    def reply_to_message(self, reply_content, receiver):
+        reply_message = Message.objects.create(sender=self.receiver, receiver=receiver, content=reply_content)
+        return reply_message
+
+    def forward_message(self, sender, receiver):
+        forwarded_message = Message.objects.create(sender=sender, receiver=receiver, content=self.content)
+        return forwarded_message
+
+
+class StudentIDField(models.PositiveIntegerField):
+
+    def get_prep_value(self, value):
+        return int(value)
+
+
+class Student(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+    student_id = StudentIDField()
+
+
+class MaskedCreditCardField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs["max_length"] = 20
+        super().__init__(*args, **kwargs)
+
+    def to_python (self, value):
+        if not isinstance(value, str):
+            raise ValidationError("The card number must be a string")
+        elif not value.isdigit():
+            raise ValidationError("The card number must contain only digits")
+        elif len(value) != 16:
+            raise ValidationError("The card number must be exactly 16 characters long")
+        return f"****-****-****-{value[-4:]}"
+
+
+class CreditCard(models.Model):
+    card_owner = models.CharField(
+        max_length=100
+    )
+    card_number = MaskedCreditCardField()
+
+
+class Hotel(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+    address = models.CharField(
+        max_length=200
+    )
+
+
+class Room(models.Model):
+    hotel = models.ForeignKey(
+        to=Hotel,
+        on_delete=models.CASCADE
+    )
+    number = models.CharField(
+        max_length=100,
+        unique=True
+    )
+    capacity = models.PositiveIntegerField()
+    total_guests = models.PositiveIntegerField()
+    price_per_night = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    def clean(self):
+        if self.total_guests > self.capacity:
+            raise ValidationError("Total guests are more than the capacity of the room")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+        return f"Room {self.number} created successfully"
+
+
+class BaseReservation(models.Model):
+    room = models.ForeignKey(
+        to=Room,
+        on_delete=models.CASCADE,
+    )
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    class Meta:
+        abstract = True
+
+    @property
+    def reserved_days(self):
+        return self.reservation_period()
+
+    @abstractmethod
+    def room_type(self):
+        return self.room_type
+
+    def reservation_period(self):
+        return (self.end_date - self.start_date).days
+
+    def calculate_total_cost(self):
+        return f"{(self.room.price_per_night * self.reserved_days):.1f}"
+
+    def clean(self):
+        if self.start_date >= self.end_date:
+            raise ValidationError("Start date cannot be after or in the same end date")
+
+        regular_reservations = self.room.regularreservation_set.all()
+        special_reservations = self.room.specialreservation_set.all()
+
+        def is_reserved_valid():
+            for reservation in regular_reservations:
+                if not (self.start_date > reservation.end_date or self.end_date < reservation.start_date):
+                    return False
+            for reservation in special_reservations:
+                if not (self.start_date > reservation.end_date or self.end_date < reservation.start_date):
+                    return False
+            return True
+
+        if not is_reserved_valid():
+            raise ValidationError(f"Room {self.room.number} cannot be reserved")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+        return f"{self.room_type} reservation for room {self.room.number}"
+
+
+class RegularReservation(BaseReservation):
+    ROOM_TYPE = "Regular"
+
+    @property
+    def room_type(self):
+        return self.ROOM_TYPE
+
+
+class SpecialReservation(BaseReservation):
+    ROOM_TYPE = "Special"
+
+    @property
+    def room_type(self):
+        return self.ROOM_TYPE
+
+    def extend_reservation(self, days: int):
+        special_reservations = self.room.regularreservation_set.all()
+        regular_reservations = self.room.specialreservation_set.all()
+
+        def check_valid_extension():
+            if self not in special_reservations or self not in regular_reservations:
+                return False
+
+            new_end_date = self.end_date + timedelta(days=days)
+
+            for reservation in special_reservations:
+                if new_end_date <= reservation.end_date:
+                    return False
+            for reservation in regular_reservations:
+                if new_end_date <= reservation.end_date:
+                    return False
+            return True
+
+        if not check_valid_extension():
+            raise ValidationError("Error during extending reservation")
+        return f"Extended reservation for room {self.room.number} with {days} days"
