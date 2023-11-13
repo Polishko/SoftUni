74b51ca4bf1e51a4e481a38330d70045a9da0ead
Niya Mateeva Yondzhju (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-11-13 21:05:47 +0200

    Update models.py
    Changes made after the lecture

--- Python - ORM/9. Advanced Queries in Django/Exercise/output/main_app/models.py
+++ Python - ORM/9. Advanced Queries in Django/Exercise/output/main_app/models.py
@@ -60,19 +60,33 @@ class Invoice(models.Model):
     invoice_number = models.CharField(max_length=20, unique=True)
     billing_info = models.OneToOneField(BillingInfo, on_delete=models.CASCADE)
 
-    @staticmethod
-    def get_invoices_with_prefix(prefix):
-        query = Q(invoice_number__startswith=prefix)
+     # join for faster queries
+   @classmethod
+    def get_invoices_with_prefix(cls, prefix):
+        return cls.objects.select_related("billing_info").filter(invoice_number__startswith=prefix)
 
-        return Invoice.objects.filter(query)
+    @classmethod
+    def get_invoices_sorted_by_number(cls):
+        return cls.objects.select_related("billing_info").order_by("invoice_number")
 
-    @staticmethod
-    def get_invoices_sorted_by_number():
-        return Invoice.objects.order_by("invoice_number")
+    @classmethod
+    def get_invoice_with_billing_info(cls, invoice_number: str):
+        return cls.objects.select_related("billing_info").get(invoice_number=invoice_number)
 
-    @staticmethod
-    def get_invoice_with_billing_info(invoice_number: str):
-        return Invoice.objects.get(invoice_number=invoice_number)
+    # these are slower    
+    # @staticmethod
+    # def get_invoices_with_prefix(prefix):
+    #     query = Q(invoice_number__startswith=prefix)
+
+    #     return Invoice.objects.filter(query)
+
+    # @staticmethod
+    # def get_invoices_sorted_by_number():
+    #     return Invoice.objects.order_by("invoice_number")
+
+    # @staticmethod
+    # def get_invoice_with_billing_info(invoice_number: str):
+    #     return Invoice.objects.get(invoice_number=invoice_number)
 
 
 class Technology(models.Model):
@@ -86,15 +100,22 @@ class Project(models.Model):
     technologies_used = models.ManyToManyField(Technology, related_name='projects')
 
     def get_programmers_with_technologies(self):
-        return self.programmers.all()
+        return self.programmers.prefetch_related("projects__technologies_used")
+        
+# # this is slow
+    # def get_programmers_with_technologies(self):
+    #     return self.programmers.all()
 
 
 class Programmer(models.Model):
     name = models.CharField(max_length=100)
     projects = models.ManyToManyField(Project, related_name='programmers')
 
-    def get_projects_with_technologies(self):
-        return self.projects.all()
+def get_projects_with_technologies(self):
+        return self.projects.prefetch_related("technologies_used")    
+
+# def get_projects_with_technologies(self):
+    #     return self.projects.all()
 
 
 class Task(models.Model):
@@ -131,13 +152,19 @@ class Task(models.Model):
 
         return Task.objects.filter(query)
 
-    @staticmethod
-    def recent_completed_tasks(days: int):
-        query = Q(is_completed=True) & Q(completion_date__gte=F("creation_date") - timedelta(days=days))
+    # @staticmethod
+    # def recent_completed_tasks(days: int):
+    #     query = Q(is_completed=True) & Q(completion_date__gte=F("creation_date") - timedelta(days=days))
+
+    #     return Task.objects.filter(query)
+
+
+    def recent_completed_tasks(self, days: int):
+        query = Q(is_completed=True) & Q(completion_date__gte=self.creation_date - timedelta(days=days))
 
         return Task.objects.filter(query)
-
-
+    
+    
 class Exercise(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=50)
@@ -145,7 +172,7 @@ class Exercise(models.Model):
     duration_minutes = models.PositiveIntegerField()
     repetitions = models.PositiveIntegerField()
 
-    @staticmethod
+    @staticmethod # or use classmethod with cls
     def get_long_and_hard_exercises():
         query = Q(duration_minutes__gt=30) & Q(difficulty_level__gte=10)
 
