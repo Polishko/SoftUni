9d1c3206465ff2ee3cdf570bb19ab14cecaa2f03
Niya Mateeva Yondzhju <119063181+Polishko@users.noreply.github.com>
2024-03-05 14:34:28 +0200

    Update qu3.js
    Added alternative solution shown in lecture

--- JS Front-End/02-JS Arrays and Strings/Exercise/qu3.js
+++ JS Front-End/02-JS Arrays and Strings/Exercise/qu3.js
@@ -1,20 +1,27 @@
 function sortAndPrint(myArray) {
-    let lowerCaseArray = myArray.map(personName => personName.toLowerCase()).sort();
-    let counter = 0;
-
-    for (loweredName of lowerCaseArray) {
-
-        while (myArray.length > 0) {
-            let current = myArray.shift();
-            if (current.toLowerCase() === loweredName) {
-                counter += 1;
-                console.log(`${counter}.${current}`);
-                break;
-            }
-            myArray.push(current);
-            }
-        }
+    let lowerCaseArray = myArray
+                            .sort((a, b) => a.localeCompare(b))
+                            .forEach((personName, idx) => console.log(`${idx + 1}.${personName}`));
     }
+
+
+// function sortAndPrint(myArray) {
+//     let lowerCaseArray = myArray.map(personName => personName.toLowerCase()).sort();
+//     let counter = 0;
+
+//     for (loweredName of lowerCaseArray) {
+
+//         while (myArray.length > 0) {
+//             let current = myArray.shift();
+//             if (current.toLowerCase() === loweredName) {
+//                 counter += 1;
+//                 console.log(`${counter}.${current}`);
+//                 break;
+//             }
+//             myArray.push(current);
+//             }
+//         }
+//     }
 
 // sortAndPrint(["John", "Bob", "Christina", "Ema"]);
 // sortAndPrint(["Bob", "abe", "Christina", "Ema"]);
