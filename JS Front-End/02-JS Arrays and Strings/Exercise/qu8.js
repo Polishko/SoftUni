ef37621034a84e059748bd1e1a7eb7b0e8fdc962
Niya Mateeva Yondzhju <119063181+Polishko@users.noreply.github.com>
2024-03-05 17:56:22 +0200

    Update qu8.js
    Added better alternative shown in lecture

--- JS Front-End/02-JS Arrays and Strings/Exercise/qu8.js
+++ JS Front-End/02-JS Arrays and Strings/Exercise/qu8.js
@@ -1,12 +1,21 @@
+// A much better alternative that can catch exact matches while the one below has extra regex to avoid catching substrings
 function splitter(myString) {
-    let regexp = /[A-Z][a-z\d]*(?=[A-Z]|$)/g;
-    // let regexp = /[A-Z][a-z\d]*/g; wont correctly catch last example
-    let allMatches = myString.match(regexp);
-
-    if (allMatches) {
-        console.log(allMatches.join(', '));
-    }
+    const regexp = /[A-Z][a-z]*/g;
+    const allMatches = myString.matchAll(regexp);
+    const words = Array.from(allMatches).map(match => match[0]);
+    
+    console.log(words.join(', '));
 }
+
+// function splitter(myString) {
+//     let regexp = /[A-Z][a-z\d]*(?=[A-Z]|$)/g;
+//     // let regexp = /[A-Z][a-z\d]*/g; wont correctly catch last example
+//     let allMatches = myString.match(regexp);
+
+//     if (allMatches) {
+//         console.log(allMatches.join(', '));
+//     }
+// }
 
 // splitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
 // splitter('HoldTheDoor');
