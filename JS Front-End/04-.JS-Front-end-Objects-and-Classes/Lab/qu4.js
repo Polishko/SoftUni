d854ff95c82f9a2f68d99e208223fde9c3b0dfc5
Niya Mateeva Yondzhju <119063181+Polishko@users.noreply.github.com>
2024-03-11 20:10:29 +0200

    Update qu4.js
    Better alternative from lecture

--- JS Front-End/04-.JS-Front-end-Objects-and-Classes/Lab/qu4.js
+++ JS Front-End/04-.JS-Front-end-Objects-and-Classes/Lab/qu4.js
@@ -1,11 +1,22 @@
 function objectToString(firstName, lastName, hairColor) {
-    let person = {};
-    person.name = firstName;
-    person.lastName = lastName;
-    person.hairColor = hairColor;
+    const person = {
+        name: firstName,
+        lastName,
+        hairColor,
+    };
+
     let text = JSON.stringify(person);
     console.log(text);
 }
+
+// function objectToString(firstName, lastName, hairColor) {
+//     let person = {};
+//     person.name = firstName;
+//     person.lastName = lastName;
+//     person.hairColor = hairColor;
+//     let text = JSON.stringify(person);
+//     console.log(text);
+// }
 
 
 // objectToString('George', 'Jones', 'Brown');
