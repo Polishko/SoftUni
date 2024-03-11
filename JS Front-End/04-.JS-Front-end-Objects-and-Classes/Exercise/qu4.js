f48d40b117237c6f0f4026187422432253e80a8d
Gizmo <nalanbilgin@gmail.com>
2024-03-11 17:58:32 +0200

    Added solutions
    Added few more solutions

---
+++ JS Front-End/04-.JS-Front-end-Objects-and-Classes/Exercise/qu4.js
@@ -0,0 +1,63 @@
+function addMovies(processesArray) {
+    class Movie {
+        constructor(name, director, date) {
+            this.name = name;
+            this.director = director;
+            this.date = date;
+        }
+    }
+
+    let movies = [];
+    for (let process of processesArray) {
+        if (process.includes('addMovie')) {
+            let movieName = process.replace('addMovie ', '').trim();
+            let movie = new Movie(movieName, '', '');
+            movies.push(movie);            
+        } else if (process.includes('directedBy')) {
+            let [movieName, movieDirector] = process.split(' directedBy ');
+            const movie = movies.find((movie) => movie.name === movieName);
+
+            if (movie) {
+                movie.director = movieDirector;
+            }            
+        } else {
+            let [movieName, movieDate] = process.split(' onDate ');
+
+            const movie = movies.find((movie) => movie.name === movieName);
+
+            if (movie) {
+                movie.date = movieDate;
+            }
+
+        }
+    }
+    
+    for (let movie of movies) {
+        if (movie.name && movie.director && movie.date) {
+            console.log(JSON.stringify(movie));
+        }
+    }
+}
+
+
+// addMovies([
+//     'addMovie Fast and Furious',
+//     'addMovie Godfather',
+//     'Inception directedBy Christopher Nolan',
+//     'Godfather directedBy Francis Ford Coppola',
+//     'Godfather onDate 29.07.2018',
+//     'Fast and Furious onDate 30.07.2018',
+//     'Batman onDate 01.08.2018',
+//     'Fast and Furious directedBy Rob Cohen'
+//     ]
+//     );
+
+addMovies([
+    'addMovie The Avengers',
+    'addMovie Superman',
+    'The Avengers directedBy Anthony Russo',
+    'The Avengers onDate 30.07.2010',
+    'Captain America onDate 30.07.2010',
+    'Captain America directedBy Joe Russo'
+    ]
+    );
---
+++ JS Front-End/04-.JS-Front-end-Objects-and-Classes/Exercise/qu5.js
@@ -0,0 +1,37 @@
+function createInventory(infoArray) {
+    class Hero {
+        constructor(name, level, items) {
+            this.name = name;
+            this.level = level;
+            this.items = items;
+        }
+
+        toString() {
+            return `Hero: ${this.name}\nlevel => ${this.level}\nitems => ${this.items.join(', ')}`;
+        }
+    }
+
+    let heroList = [];
+    for (let log of infoArray) {
+        const [heroName, heroLevel, ...heroItems] = log.split(' / ');
+        heroList.push(new Hero(heroName, heroLevel, heroItems));
+    }
+
+    heroList.sort((a, b) => a.level - b.level);
+    return heroList.join('\n');
+}
+
+
+// console.log(createInventory([
+//     'Isacc / 25 / Apple, GravityGun',
+//     'Derek / 12 / BarrelVest, DestructionSword',
+//     'Hes / 1 / Desolator, Sentinel, Antara'
+//     ]
+//     ));
+
+console.log(createInventory([
+    'Batman / 2 / Banana, Gun',
+    'Superman / 18 / Sword',
+    'Poppy / 28 / Sentinel, Antara'
+    ])
+    );
