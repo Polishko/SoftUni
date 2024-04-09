b786bb7722c7547ac9478ff4b8d56e15406a4c8c
Niya Mateeva Yondzhju <119063181+Polishko@users.noreply.github.com>
2024-04-09 06:58:05 +0300

    Update app.js
    Added corrected solution

--- JS Front-End/Exams/20 Apr 2023/01. Horse Racing/app.js
+++ JS Front-End/Exams/20 Apr 2023/01. Horse Racing/app.js
@@ -1,83 +1,50 @@
-// needs correction
 function solve(arrayInput) {
     const [horseInfo, ...commands] = arrayInput;
     let horses = horseInfo.split('|');
 
     let position;
     let newPosition;
-    for  (const commandInfo of commands) {
+    for (const commandInfo of commands) {
         const [command, ...rest] = commandInfo.split(' ');
 
-        switch (command) {
-            case 'Retake':
-                let [overtaking, overtaken] = rest;
-                const idx1 = horses.indexOf(overtaking);
-                const idx2 = horses.indexOf(overtaken)
-                if (idx1 < idx2) {
-                    horses[idx1] = overtaken;
-                    horses[idx2] = overtaking;
-                    console.log(`${overtaking} retakes ${overtaken}.`);
-                }
-                break;
-            case 'Trouble':
-                const troubledHorse = rest[0];
+        if (command === 'Finish') {
+            break;
+        } else if (command === 'Retake') {
+            let [overtaking, overtaken] = rest;
+            const idx1 = horses.indexOf(overtaking);
+            const idx2 = horses.indexOf(overtaken)
+            if (idx1 < idx2) {
+                [horses[idx1], horses[idx2]] = [horses[idx2], horses[idx1]]
+                console.log(`${overtaking} retakes ${overtaken}.`);
+            }
+        } else if (command === 'Trouble') {
+            const troubledHorse = rest[0];
+            if (horses.includes(troubledHorse)) {
                 position = horses.indexOf(troubledHorse);
                 if (position !== 0) {
                     horses.splice(position, 1);
                     horses.splice(position - 1, 0, troubledHorse);
                     console.log(`Trouble for ${troubledHorse} - drops one position.`);
                 }
-                break;
-            case 'Rage':
-                const ragedHorse = rest[0];
-                position = horses.indexOf(ragedHorse);
-                if (position !== horses.length - 1) {
-                    newPosition= Math.min(position + 2, horses.length - 1);
-                    horses.splice(position, 1);
-                    horses.splice(newPosition, 0, ragedHorse);
-                    console.log(`${ragedHorse} rages 2 positions ahead.`);
-                }
-                break;
-            case 'Miracle':
-                const lastHorse = horses.shift();
-                horses.push(lastHorse);
-                console.log(`What a miracle - ${lastHorse} becomes first.`);
-                break;
-            case 'Finish':
-                console.log(horses.join('->'));
-                let winner = horses.pop();
-                if (winner) {
-                    console.log(`The winner is: ${winner}`);
-                }
-                return;
+            }
+        } else if (command === 'Rage') {
+            const ragedHorse = rest[0];
+            position = horses.indexOf(ragedHorse);
+            if (horses.includes(ragedHorse)) {
+                newPosition = Math.min(position + 2, horses.length - 1);
+                horses.splice(position, 1);
+                horses.splice(newPosition, 0, ragedHorse);
+                console.log(`${ragedHorse} rages 2 positions ahead.`);
+            }
+        } else if (command === 'Miracle') {
+            const lastHorse = horses.shift();
+            horses.push(lastHorse);
+            console.log(`What a miracle - ${lastHorse} becomes first.`);
         }
     }
+    console.log(horses.join('->'));
+    let winner = horses.pop();
+    if (winner) {
+        console.log(`The winner is: ${winner}`);
+    }
 }
-
-// solve((['Bella|Alexia|Sugar',
-//     'Retake Alexia Sugar',
-//     'Rage Bella',
-//     'Trouble Bella',
-//     'Finish'])
-// );
-
-// solve((['Onyx|Domino|Sugar|Fiona',
-//     'Trouble Onyx',
-//     'Retake Onyx Sugar',
-//     'Rage Domino',
-//     'Miracle',
-//     'Finish'])
-// );
-
-// solve((['Fancy|Lilly',
-//     'Retake Lilly Fancy',
-//     'Trouble Lilly',
-//     'Trouble Lilly',
-//     'Finish',
-//     'Rage Lilly'])
-// );
-
-solve((['',
-    'Finish',
-    ])
-);
