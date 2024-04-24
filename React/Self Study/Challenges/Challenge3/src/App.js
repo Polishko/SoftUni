1dc91c54d2a008e3c69af9e16d4cc62e0977f2e8
Niya Mateeva Yondzhju <119063181+Polishko@users.noreply.github.com>
2024-04-24 11:28:50 +0300

    Update App.js
    Improved code

--- React/Self Study/Challenges/Challenge3/src/App.js
+++ React/Self Study/Challenges/Challenge3/src/App.js
@@ -13,35 +13,88 @@ function Counter() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
-  const days = step * count;
+  const date = new Date();
+  date.setDate(date.getDate() + count);
 
-  const currentDate = new Date();
-  currentDate.setDate(currentDate.getDate() + days);
-  const formattedDate = currentDate.toDateString();
-
-  let message = `Today is ${formattedDate}`;
-  if (count < 0) message = `${Math.abs(days)} ago was ${formattedDate}`;
-  if (count > 0) message = `${days} days from today is ${formattedDate}`;
+  let message = `Today is `;
+  if (count < 0)
+    message = `${Math.abs(count)} ${
+      Math.abs(count) === 1 ? "day" : "days"
+    } ago was `;
+  if (count > 0)
+    message = `${count} ${count === 1 ? "day" : "days"} from today is `;
 
   return (
     <>
-      <p className="step">
+      <div>
         <button
           onClick={() => {
-            if (step > 1) setStep((s) => s - 1);
+            if (step > 1) setStep((c) => c - step);
           }}
         >
           -
         </button>
-        {`Step: ${step}`}
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
-      </p>
-      <p className="count">
+        <span>{`Step: ${step}`}</span>
+        <button onClick={() => setStep((c) => c + step)}>+</button>
+      </div>
+      <div>
         <button onClick={() => setCount((s) => s - 1)}>-</button>
-        {`Count: ${count}`}
+        <span>{`Count: ${count}`}</span>
         <button onClick={() => setCount((s) => s + 1)}>+</button>
+      </div>
+      <p>
+        <span>{message}</span>
+        <span>{date.toDateString()}</span>
       </p>
-      <p className="result">{message}</p>
     </>
   );
 }
+
+// // Initial solution
+// import "./styles.css";
+// import { useState } from "react";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <Counter />
+//     </div>
+//   );
+// }
+
+// function Counter() {
+//   const [step, setStep] = useState(1);
+//   const [count, setCount] = useState(0);
+
+//   const days = step * count;
+
+//   const currentDate = new Date();
+//   currentDate.setDate(currentDate.getDate() + days);
+//   const formattedDate = currentDate.toDateString();
+
+//   let message = `Today is ${formattedDate}`;
+//   if (count < 0) message = `${Math.abs(days)} ago was ${formattedDate}`;
+//   if (count > 0) message = `${days} days from today is ${formattedDate}`;
+
+//   return (
+//     <>
+//       <p className="step">
+//         <button
+//           onClick={() => {
+//             if (step > 1) setStep((s) => s - 1);
+//           }}
+//         >
+//           -
+//         </button>
+//         {`Step: ${step}`}
+//         <button onClick={() => setStep((s) => s + 1)}>+</button>
+//       </p>
+//       <p className="count">
+//         <button onClick={() => setCount((s) => s - 1)}>-</button>
+//         {`Count: ${count}`}
+//         <button onClick={() => setCount((s) => s + 1)}>+</button>
+//       </p>
+//       <p className="result">{message}</p>
+//     </>
+//   );
+// }
