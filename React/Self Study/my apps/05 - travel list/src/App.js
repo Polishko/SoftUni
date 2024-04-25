ba23f490638e499eb0427918fb4887cd878368ee
Gizmo <nalanbilgin@gmail.com>
2024-04-25 21:38:50 +0300

    Started new app
    Started new app

---
+++ React/Self Study/my apps/05 - travel list/src/App.js
@@ -0,0 +1,62 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>"What do you need for your 😍 trip?"</h3>
+      <select>{Array.from({ length: 20 }, (_, i) => i + 1)}</select>
+      <input type="text" placeholder="Item..."></input>
+      <button>Add</button>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
