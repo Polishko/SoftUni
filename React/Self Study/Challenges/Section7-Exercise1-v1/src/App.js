2ec0501c0743e469c74e2b48bff06b7b055779fb
Niya Mateeva Yondzhju <119063181+Polishko@users.noreply.github.com>
2024-04-30 17:36:57 +0300

    Update App.js
    Added lecturer's soution

--- React/Self Study/Challenges/Section7-Exercise1-v1/src/App.js
+++ React/Self Study/Challenges/Section7-Exercise1-v1/src/App.js
@@ -1,3 +1,4 @@
+// Solution after lecture
 import "./styles.css";
 import { useState } from "react";
 
@@ -19,40 +20,100 @@ const faqs = [
 export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-function Accordion() {
+function Accordion({ data }) {
   return (
-    <div className="content-box">
-      <ul className="accordion">
-        {faqs.map((faq, index) => (
-          <Item item={faq} index={index} key={index} />
-        ))}
-      </ul>
-    </div>
+    <ul className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem
+          title={el.title}
+          number={i}
+          text={el.text}
+          key={el.title}
+        />
+      ))}
+    </ul>
   );
 }
 
-function Item({ item, index }) {
-  const [openStatus, setOpenStatus] = useState(false);
+function AccordionItem({ title, text, number }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-  function handleClick() {
-    setOpenStatus((openStatus) => !openStatus);
+  function handleToggle() {
+    setIsOpen((openStatus) => !openStatus);
   }
 
   return (
-    <li className={`item ${openStatus ? "open" : ""}`}>
-      <span className="number">{`0${index + 1}`}</span>
-      <div>
-        <p className="title">{item.title}</p>
-        {openStatus === true && <p>{item.text}</p>}
-      </div>
-      <span className="icon" onClick={handleClick}>
-        {openStatus ? "-" : "+"}
-      </span>
+    <li className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{number < 9 ? `0${number + 1}` : number + 1}</p>
+      <p className="text">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
     </li>
   );
 }
+
+
+// Initial solution
+// import "./styles.css";
+// import { useState } from "react";
+
+// const faqs = [
+//   {
+//     title: "Where are these chairs assembled?",
+//     text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+//   },
+//   {
+//     title: "How long do I have to return my chair?",
+//     text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+//   },
+//   {
+//     title: "Do you ship to countries outside the EU?",
+//     text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+//   },
+// ];
+
+// export default function App() {
+//   return (
+//     <div>
+//       <Accordion />
+//     </div>
+//   );
+// }
+
+// function Accordion() {
+//   return (
+//     <div className="content-box">
+//       <ul className="accordion">
+//         {faqs.map((faq, index) => (
+//           <Item item={faq} index={index} key={index} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// function Item({ item, index }) {
+//   const [openStatus, setOpenStatus] = useState(false);
+
+//   function handleClick() {
+//     setOpenStatus((openStatus) => !openStatus);
+//   }
+
+//   return (
+//     <li className={`item ${openStatus ? "open" : ""}`}>
+//       <span className="number">{`0${index + 1}`}</span>
+//       <div>
+//         <p className="title">{item.title}</p>
+//         {openStatus === true && <p>{item.text}</p>}
+//       </div>
+//       <span className="icon" onClick={handleClick}>
+//         {openStatus ? "-" : "+"}
+//       </span>
+//     </li>
+//   );
+// }
