125831739892857f2ac7986917374bd25242f04c
Niya Mateeva Yondzhju <119063181+Polishko@users.noreply.github.com>
2024-04-30 12:11:13 +0300

    Update App.js
    Update: Added functionality to clear the list of items

--- React/Self Study/my apps/05 - travel list/src/App.js
+++ React/Self Study/my apps/05 - travel list/src/App.js
@@ -25,6 +25,15 @@ export default function App() {
     );
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm(
+      //Prevent accidental deletiin by adding DOM/browser confirmation
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -33,6 +42,7 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
       />
       <Stats items={items} />
     </div>
@@ -91,7 +101,7 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
   const [sortBy, setSortBy] = useState("input"); // can be also done in the App component and passed down
   let sortedItems;
 
@@ -125,6 +135,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
+        <button onClick={onClearList}>Clear list</button>
       </div>
     </div>
   );
