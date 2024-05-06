457ad0f729bfc81c01e2e3a8bb685bb434777346
Gizmo <nalanbilgin@gmail.com>
2024-05-06 17:40:14 +0300

    Completed the app
    Completed my first mini app

--- React/Self Study/my apps/mini-cat-app/src/App.js
+++ React/Self Study/my apps/mini-cat-app/src/App.js
@@ -1,25 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const initialCats = [
   {
     id: 118836,
     name: "Gizmo",
     image: "https://cataas.com/cat?u=118836",
-    happiness: 100,
+    hunger: 0,
+    litterDirtyness: 0,
   },
 
   {
     id: 245672,
     name: "Kaju",
     image: "https://cataas.com/cat?u=933372",
-    happiness: 50,
+    hunger: 0,
+    litterDirtyness: 0,
   },
 
   {
     id: 499476,
     name: "Polen",
     image: "https://cataas.com/cat?u=499476",
-    happiness: 30,
+    hunger: 0,
+    litterDirtyness: 0,
   },
 ];
 
@@ -34,6 +37,8 @@ function Button({ children, className, onClick }) {
 export default function App() {
   const [cats, setCats] = useState(initialCats);
   const [showAddCat, setShowAddCat] = useState(false);
+  const [selectedCat, setSelectedCat] = useState(null);
+  const [adopted, setAdopted] = useState(0);
 
   function handleShowAddCat() {
     setShowAddCat((show) => !show);
@@ -43,48 +48,174 @@ export default function App() {
     setCats((cats) => [...cats, cat]);
   }
 
+  useEffect(() => {
+    const hungerTimer = setInterval(() => {
+      setCats((prevCats) =>
+        prevCats.map((cat) => {
+          const newHunger = cat.hunger + 1.5;
+          if (newHunger >= 100) {
+            return { ...cat, hunger: 100 };
+          } else {
+            return { ...cat, hunger: newHunger };
+          }
+        })
+      );
+    }, 10000);
+
+    return () => clearInterval(hungerTimer);
+  }, []);
+
+  useEffect(() => {
+    const dirdinessTimer = setInterval(() => {
+      setCats((prevCats) =>
+        prevCats.map((cat) => {
+          const newDirdiness = cat.litterDirtyness + 0.5;
+          if (newDirdiness >= 100) {
+            return { ...cat, litterDirtyness: 100 };
+          } else {
+            return { ...cat, litterDirtyness: newDirdiness };
+          }
+        })
+      );
+    }, 30000);
+
+    return () => clearInterval(dirdinessTimer);
+  }, []);
+
+  function handleFeed(currCat) {
+    setCats((prevCats) =>
+      prevCats.map((cat) =>
+        cat.id === currCat.id ? { ...cat, hunger: 0 } : cat
+      )
+    );
+  }
+
+  function handleCleanLitter(currCat) {
+    setCats((prevCats) =>
+      prevCats.map((cat) =>
+        cat.id === currCat.id ? { ...cat, litterDirtyness: 0 } : cat
+      )
+    );
+  }
+
+  function handleSelection(cat) {
+    setSelectedCat(cat);
+  }
+
+  function handleCloseSelected() {
+    setSelectedCat(null);
+  }
+
+  function handleAdoptCat() {
+    setAdopted((curr) => curr + 1);
+    setCats((cats) => cats.filter((cat) => cat.id !== selectedCat.id));
+    setSelectedCat(null);
+  }
+
   return (
     <div className="app">
-      <div className="container-1">
-        <CatList cats={cats} />
+      <div className="shelter-cats">
+        <CatList
+          cats={cats}
+          onFeed={handleFeed}
+          onLitterClean={handleCleanLitter}
+          onSelection={handleSelection}
+          showAddCat={showAddCat}
+          selectedCat={selectedCat}
+        />
 
         {showAddCat && <AddNewCat onAddCat={handleAddCat} />}
 
-        <Button
-          className={!showAddCat ? "button add pink" : "button add blue"}
-          onClick={handleShowAddCat}
-        >
-          {!showAddCat ? "Add cat" : "Close"}
-        </Button>
+        {!selectedCat && (
+          <Button
+            className={
+              !showAddCat ? "button add-close pink" : "button add-close blue"
+            }
+            onClick={handleShowAddCat}
+          >
+            {!showAddCat ? "Add cat" : "Close and manage cats"}
+          </Button>
+        )}
+        <ShelterStats cats={cats} adopted={adopted} setAdopted={setAdopted} />
       </div>
+
+      {selectedCat && (
+        <ManageCat
+          selectedCat={selectedCat}
+          onFeed={handleFeed}
+          onLitterClean={handleCleanLitter}
+          onCloseSelected={handleCloseSelected}
+          onAdoptCat={handleAdoptCat}
+        />
+      )}
     </div>
   );
 }
 
-function CatList({ cats }) {
+function CatList({ cats, onSelection, showAddCat, selectedCat }) {
   return (
     <ul className="cat-list">
       {cats.map((cat) => (
-        <Cat cat={cat} key={cat.id} />
+        <Cat
+          cat={cat}
+          key={cat.id}
+          onSelection={onSelection}
+          showAddCat={showAddCat}
+          selectedCat={selectedCat}
+        />
       ))}
     </ul>
   );
 }
 
-function Cat({ cat }) {
+function Cat({ cat, onSelection, showAddCat, selectedCat }) {
   return (
-    <li className="cat">
+    <li className={selectedCat?.id === cat.id ? "cat selected" : "cat"}>
       <div className="cat-img">
         <img src={cat.image} alt={cat.name} />
       </div>
       <div className="cat-info">
         <h3>{cat.name}</h3>
-        <p>
-          {cat.name}'s happiness level is {cat.happiness}
-        </p>
+        <div>
+          <p>
+            {cat.name}'s hunger level is {Math.abs(cat.hunger)}%
+          </p>
+          <p>
+            {cat.name}'s litter dirtiness level is{" "}
+            {Math.abs(cat.litterDirtyness)}%
+          </p>
+
+          {cat.hunger === 100 && cat.litterDirtyness === 100 && (
+            <p>{cat.name} is unhappy 😿! Feed cat and change litter!</p>
+          )}
+
+          {cat.hunger === 100 && cat.litterDirtyness < 100 && (
+            <p>{cat.name} is unhappy 😿! Feed cat!</p>
+          )}
+
+          {cat.hunger < 100 && cat.litterDirtyness === 100 && (
+            <p>{cat.name} is uhappy 😿! Change litter!</p>
+          )}
+
+          {cat.hunger < 100 && cat.litterDirtyness < 100 && (
+            <p>{cat.name} is happy 😻!</p>
+          )}
+        </div>
       </div>
-      <Button className="button feed green">Feed</Button>
-      <Button className="button clean-litter blue">Clean litter</Button>
+
+      <Button
+        className={showAddCat ? "button disabled no-hover" : "button blue"}
+        onClick={(e) => {
+          if (showAddCat) {
+            e.preventDefault();
+            e.stopPropagation();
+          } else {
+            onSelection(cat);
+          }
+        }}
+      >
+        Manage
+      </Button>
     </li>
   );
 }
@@ -99,7 +230,8 @@ function AddNewCat({ onAddCat }) {
     id,
     name,
     image: `${image}?u=${id}`,
-    happiness: 100,
+    hunger: 100,
+    litterDirtyness: 0,
   };
 
   function handleAddCat(e) {
@@ -127,7 +259,61 @@ function AddNewCat({ onAddCat }) {
         value={image}
         onChange={(e) => setImage(e.target.value)}
       />
-      <Button className="button add pink">Add</Button>
+      <Button className="button add-close pink">Add</Button>
     </form>
+  );
+}
+
+function ManageCat({
+  selectedCat,
+  onFeed,
+  onLitterClean,
+  onCloseSelected,
+  onAdoptCat,
+}) {
+  return (
+    <div className="manage-cat">
+      <h3>{selectedCat?.name}</h3>
+
+      <div className="manage-1">
+        <Button
+          className="button feed green"
+          onClick={() => onFeed(selectedCat)}
+        >
+          Feed
+        </Button>
+        <Button
+          className="button clean-litter blue"
+          onClick={() => onLitterClean(selectedCat)}
+        >
+          Clean
+        </Button>
+      </div>
+      <div className="manage-2">
+        <Button className="button adopt blue" onClick={onAdoptCat}>
+          Adopt
+        </Button>
+        <Button className="button pink" onClick={onCloseSelected}>
+          Close
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+function ShelterStats({ cats, adopted }) {
+  const countCats = cats?.length;
+
+  if (!countCats)
+    return (
+      <p className="all-adopted">Congratulations! All cats are adopted!</p>
+    );
+
+  return (
+    <div className="shelter-stats">
+      <h3>Shelter information</h3>
+      <p>Cats in shelter: {countCats}</p>
+      <p>Adoted cats: {adopted}</p>
+    </div>
   );
 }
