b5bc73f5953198c18b4a6f76be5efd6f528b43d0
Gizmo <nalanbilgin@gmail.com>
2024-05-09 15:43:50 +0300

    Added new component
    Added new customizable component

--- React/Self Study/my apps/07- usepopcorn/src/index.js
+++ React/Self Study/my apps/07- usepopcorn/src/index.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color={"blue"} maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rared {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={10}
+      messages={["Terrible", "Bad", "Ok", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
