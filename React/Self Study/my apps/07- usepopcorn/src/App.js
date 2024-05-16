2a97baddd5ce4d8fad3537cbfaefcaf964e24a69
Gizmo <nalanbilgin@gmail.com>
2024-05-16 12:54:03 +0300

    Added simple loader
    Added simple loader to show during data fetching

--- React/Self Study/my apps/07- usepopcorn/src/App.js
+++ React/Self Study/my apps/07- usepopcorn/src/App.js
@@ -191,6 +191,7 @@ const KEY = "8d22a2eb";
 export default function App() {
   const [watched, setWatched] = useState([]);
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = "terminator";
 
   // useEffect(function () {
@@ -201,11 +202,13 @@ export default function App() {
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
+      setIsLoading(false);
     }
     fetchMovies();
   }, []);
@@ -217,9 +220,7 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <WatchedSummary watched={watched} />
@@ -228,6 +229,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 function NavBar({ children }) {
