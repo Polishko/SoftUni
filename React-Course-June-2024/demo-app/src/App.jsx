36aa9ccdf3a2ffaa9d433215f1496ba99ef6f4c1
Gizmo <nalanbilgin@gmail.com>
2024-06-27 09:31:11 +0300

    Added demo practice
    Added demo practice

---
+++ React-Course-June-2024/demo-app/src/App.jsx
@@ -0,0 +1,19 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Intro from "./components/Intro";
+import Menu from "./components/Menu";
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Intro />
+      <Menu />
+      <About />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
