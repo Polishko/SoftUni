405c30bf114ddbbd579b381c66730a23c5646ff8
Gizmo <nalanbilgin@gmail.com>
2024-09-13 20:24:59 +0300

    Configured database with environment variables

--- Django Basics/djangoIntroduction/djangoIntroduction/settings.py
+++ Django Basics/djangoIntroduction/djangoIntroduction/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,14 +80,14 @@ WSGI_APPLICATION = 'djangoIntroduction.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "todo_db",
-        "USER": "postgres",
-        "PASSWORD": "password",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
+    },
 }
 
 # Password validation
--- Django Basics/djangoIntroduction/djangoIntroduction/todo_app/views.py
+++ Django Basics/djangoIntroduction/djangoIntroduction/todo_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Task
+
 
 # Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    print(tasks)
+
+    result = '\n'.join([
+        '<ul>',
+        *[f'<li>{task}<li>' for task in tasks],
+        '</ul>'
+    ])
+    return HttpResponse(result)
+
