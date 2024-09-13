827255d57039f7ef26f1ae60ba0e3e94521c8ec6
Gizmo <nalanbilgin@gmail.com>
2024-09-13 20:42:51 +0300

    registered model in admin

--- Django Basics/djangoIntroduction/djangoIntroduction/todo_app/admin.py
+++ Django Basics/djangoIntroduction/djangoIntroduction/todo_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from djangoIntroduction.todo_app.models import Task
+
+
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
