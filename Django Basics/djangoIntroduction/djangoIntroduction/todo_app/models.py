60857657bd88274f53ca6a95526578cefe02b6fb
Gizmo <nalanbilgin@gmail.com>
2024-09-13 18:02:15 +0300

    added a model

--- Django Basics/djangoIntroduction/djangoIntroduction/todo_app/models.py
+++ Django Basics/djangoIntroduction/djangoIntroduction/todo_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    name = models.CharField(
+        max_length=50
+    )
+
+    description = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
