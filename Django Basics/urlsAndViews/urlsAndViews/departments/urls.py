789a679e42c1790ce37b77ddcc4a257588e76474
Gizmo <nalanbilgin@gmail.com>
2024-09-17 21:09:01 +0300

    prev project commits

--- Django Basics/urlsAndViews/urlsAndViews/departments/urls.py
+++ Django Basics/urlsAndViews/urlsAndViews/departments/urls.py
@@ -4,6 +4,7 @@ from urlsAndViews.departments import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:pk>/', views.view_with_int_pk),
     path('<param>/', views.view_with_name),
 ]
 
--- Django Basics/urlsAndViews/urlsAndViews/departments/views.py
+++ Django Basics/urlsAndViews/urlsAndViews/departments/views.py
@@ -8,9 +8,13 @@ def index(request):
 
 
 # catch with *kwargs
-def view_with_name(request, *args, **kwargs):
-    return HttpResponse(f'<h1>Kwargs: {kwargs}</h1>')
+# def view_with_name(request, *args, **kwargs):
+#     return HttpResponse(f'<h1>Kwargs: {kwargs}</h1>')
 
 # catch with url variable name
-# def view_with_name(request, param):
-#     return HttpResponse(f'<h1>Kwargs: {param}</h1>')
+def view_with_name(request, param):
+    return HttpResponse(f'<h1>Variable: {param}</h1>')
+
+
+def view_with_int_pk(request, pk):
+    return HttpResponse(f'<h1>Int pk with pk: {pk}</h1>')
