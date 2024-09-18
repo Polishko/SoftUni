8f38056db79dc6864acbc461e17a801bbaa9c830
Gizmo <nalanbilgin@gmail.com>
2024-09-18 11:31:13 +0300

    iclude example

--- Django Basics/urlsAndViews/urlsAndViews/departments/urls.py
+++ Django Basics/urlsAndViews/urlsAndViews/departments/urls.py
@@ -1,12 +1,14 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from urlsAndViews.departments import views
 
 
 urlpatterns = [
     path('', views.index),
     path('<int:pk>/', views.view_with_int_pk),
-    path('<int:pk>/<slug:slug>', views.view_with_slug),
-    path('<variable>/', views.view_with_name),
+    path('numbers/', include([
+        path('<int:pk>/<slug:slug>', views.view_with_slug),
+        path('<variable>/', views.view_with_name),
+    ])),
     re_path(r'^archive/(?P<archive_year>202[0-3])/$', views.show_archive),
     # path('<path:variable>/', views.view_with_name),
     # path('<uuid:id>/'),
