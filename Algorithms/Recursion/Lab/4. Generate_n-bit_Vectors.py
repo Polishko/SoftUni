a5674a19d963c1542b7ecdc4af3ab09402397e74
Gizmo <nalanbilgin@gmail.com>
2024-09-21 18:52:25 +0300

    Added solution for 4th problem

---
+++ Algorithms/Recursion/Lab/4. Generate_n-bit_Vectors.py
@@ -0,0 +1,12 @@
+def gen01(idx, vector):
+    if idx >= len(vector):
+        print(*vector, sep=' ')
+        return
+
+    for i in range(2):
+        vector[idx] = i
+        gen01(idx + 1, vector)
+
+n = int(input("Enter n: "))
+vector = [0] * n
+gen01(0, vector)
