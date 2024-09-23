99e3f81583836ea0ab3171129b05994726e811d5
Gizmo <nalanbilgin@gmail.com>
2024-09-23 13:59:39 +0300

    added tag examples and created enew mpty y view

--- Django Basics/templatesBasics/templatesBasics/posts/urls.py
+++ Django Basics/templatesBasics/templatesBasics/posts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from templatesBasics.posts.views import index
-
+from templatesBasics.posts.views import index, dashboard
 
 urlpatterns = [
     path('', index, name='index'),
+    path('dashboard/', dashboard, name='dashboard'),
 ]
--- Django Basics/templatesBasics/templatesBasics/posts/views.py
+++ Django Basics/templatesBasics/templatesBasics/posts/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from lib2to3.fixes.fix_input import context
 
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -14,7 +15,41 @@ def  index(request):
         },
         'ids': ['21348', '485769', '619074'],
         'some_text': 'hello, my name is Nalan, and I`m learning programming.',
-        'empty_text': ''
+        'empty_text': '',
+        'users': [
+            'pesho',
+            'maria',
+            'ivan',
+            'magdalena',
+            'elisa',
+        ]
     }
 
     return render(request, 'base.html', context)
+
+def dashboard(request):
+    context = {
+        'posts':[
+            {
+                'title': 'How to create a Django project 1?',
+                'author': 'Nalan',
+                'content': 'I know a little about creating a Django project.',
+                'created_at': datetime.now(),
+            },
+            {
+                'title': 'How to create a Django project 2?',
+                'author': 'Nalan',
+                'content': 'I know a little about creating a Django project.',
+                'created_at': datetime.now(),
+            },
+            {
+                'title': 'How to create a Django project 3?',
+                'author': 'Nalan',
+                'content': 'I know a little about creating a Django project.',
+                'created_at': datetime.now(),
+            },
+
+        ]
+    }
+
+    return render(request, 'posts/dashboard.html', context)
