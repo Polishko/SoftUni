8a048a45e2fe3d535c8246f0e706af2d4d4db844
Gizmo <nalanbilgin@gmail.com>
2024-10-07 20:38:02 +0300

    Added third part of workshop

---
+++ Django Basics/Workshop-Petstagram/Petstagram/pets/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+
+from Petstagram.pets.mixins import DisableFieldsMixin
+from Petstagram.pets.models import Pet
+
+class PetBaseForm(forms.ModelForm):
+    class Meta:
+        model = Pet
+        fields = ['name', 'date_of_birth', 'personal_photo']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Pet name'}),
+            'date_of_birth': forms.DateInput(attrs={'placeholder': 'Date'}),
+            'personal_photo': forms.TextInput(attrs={'placeholder': 'Pet image'}),
+        }
+        labels = {
+            'name': 'Pet Name',
+            'date_of_birth': 'Date of Birth',
+            'personal_photo': 'Link to Image',
+        }
+
+class PetCreateForm(PetBaseForm):
+    pass
+
+class PetEditForm(PetBaseForm):
+    pass
+
+class PetDeleteForm(PetBaseForm, DisableFieldsMixin):
+    disabled_fields = ('__all__',)
+    readonly_fields = ('__all__',)
--- Django Basics/Workshop-Petstagram/Petstagram/pets/views.py
+++ Django Basics/Workshop-Petstagram/Petstagram/pets/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
 
+from django.shortcuts import render, redirect, get_object_or_404
+
+from Petstagram.pets.forms import PetCreateForm, PetEditForm, PetDeleteForm
 from Petstagram.pets.models import Pet
-from Petstagram.photos.models import Photo
 
 
 # Create your views here.
-def pet_add_page(request):
-    return render(request, template_name='pets/pet-add-page.html')
-
 def pet_details_page(request, username: str, pet_slug: str):
     pet = Pet.objects.get(slug=pet_slug)
     # pet = get_object_or_404(Pet, slug=pet_slug)
@@ -19,8 +18,51 @@ def pet_details_page(request, username: str, pet_slug: str):
     }
     return render(request, template_name='pets/pet-details-page.html', context=context)
 
+
+def pet_add_page(request):
+    form = PetCreateForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('profile-details', pk=1)
+
+    context = {'form': form}
+
+    return render(request, template_name='pets/pet-add-page.html', context=context)
+
+
 def pet_edit_page(request, username: str, pet_slug: str):
-    return render(request, template_name='pets/pet-edit-page.html')
+    pet = get_object_or_404(Pet, slug=pet_slug)
+    form = PetDeleteForm(request.POST or None, instance=pet)
+
+    if request.method == 'POST' and form.is_valid():
+            form.save()
+            return redirect('pet-details', username, pet_slug)
+
+    # if request.method == 'GET':
+    #     # form = PetEditForm(instance=pet, initial=pet.__dict__)
+    #     form = PetEditForm(instance=pet)
+    # else:
+    #     form = PetEditForm(request.POST, instance=pet)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('pet-details', username, pet_slug)
+
+    context = {'form': form}
+
+    return render(request, template_name='pets/pet-edit-page.html', context=context)
+
 
 def pet_delete_page(request, username: str, pet_slug: str):
-    return render(request, template_name='pets/pet-delete-page.html')
+    pet = get_object_or_404(Pet, slug=pet_slug)
+    form = PetDeleteForm(request.POST or None, instance=pet)
+
+    if request.method == 'POST':
+        pet.delete()
+        return redirect('profile-details', pk=1)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, template_name='pets/pet-delete-page.html', context=context)
