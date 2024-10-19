e98df2eef4b0eb4a8e5534655add2e6d3285aada
Gizmo <nalanbilgin@gmail.com>
2024-10-19 16:19:15 +0300

    Added some explanatory comments after finishing lecture

--- Django Basics/Workshop-Petstagram/Petstagram/common/templatetags/pass_query.py
+++ Django Basics/Workshop-Petstagram/Petstagram/common/templatetags/pass_query.py
@@ -4,6 +4,6 @@ register = template.Library()
 
 @register.simple_tag
 def url_replace(request, field, value):
-    dict_ = request.GET.copy()  # Get the current query parameters
-    dict_[field] = value  # Update the specified field with the new value
-    return dict_.urlencode()  # Return the updated query string
+    dict_ = request.GET.copy()  # Get the current query parameters and copy {'pet_name': 'george'}
+    dict_[field] = value  # Add the new field and value to dict {'pet_name': 'george', 'page': 2}
+    return dict_.urlencode()  # Return the updated query string ?pet_name=george&page=2
--- Django Basics/Workshop-Petstagram/Petstagram/photos/views.py
+++ Django Basics/Workshop-Petstagram/Petstagram/photos/views.py
@@ -18,7 +18,6 @@ class PhotoDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # photo = self.get_object()
         photo = self.object
         context['comments'] = photo.comments.all()
         context['likes'] = photo.likes.all()
