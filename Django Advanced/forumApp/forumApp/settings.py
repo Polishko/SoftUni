851ff8b3fded847327a1a312342a37b3fa7aab83
Gizmo <nalanbilgin@gmail.com>
2024-11-01 15:48:06 +0200

    Added redirect link for login and an arrow for login in menu

--- Django Advanced/forumApp/forumApp/settings.py
+++ Django Advanced/forumApp/forumApp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,3 +149,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = reverse_lazy('index')
