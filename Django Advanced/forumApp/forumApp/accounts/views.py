866943f2bab328a4ab2e4308344b99c751d4310a
Gizmo <nalanbilgin@gmail.com>
2024-11-01 13:34:15 +0200

    Compleded registration view

--- Django Advanced/forumApp/forumApp/accounts/views.py
+++ Django Advanced/forumApp/forumApp/accounts/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 
 class UserRegisterView(CreateView):
     form_class = UserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('index')
     
