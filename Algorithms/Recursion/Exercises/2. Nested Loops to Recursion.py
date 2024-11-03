194cb98bb14e8e8346e5778f562264b679f5195b
Gizmo <nalanbilgin@gmail.com>
2024-11-03 13:47:24 +0200

    Added lecturers solution to Qu2.

--- Algorithms/Recursion/Exercises/2. Nested Loops to Recursion.py
+++ Algorithms/Recursion/Exercises/2. Nested Loops to Recursion.py
@@ -1,14 +1,28 @@
+# Lecturer`s solution
 
-
-def find_iteration_variables(n, result):
-    if len(result) == n:
-        print(' '.join(str(x) for x in result))
+def nested_loops(idx, arr):
+    if idx >= len(arr):
+        print(*arr, sep = ' ')
         return
 
-    for i in range(1, n + 1):
-        result.append(i)
-        find_iteration_variables(n, result)
-        result.pop()
+    for num in range(1, len(arr) + 1):
+        arr[idx] = num
+        nested_loops(idx + 1, arr)
+
+n = int(input())
+array = [None] * n
+nested_loops(0, array)
 
 
-find_iteration_variables(int(input()), [])
+# def find_iteration_variables(n, result):
+#     if len(result) == n:
+#         print(' '.join(str(x) for x in result))
+#         return
+#
+#     for i in range(1, n + 1):
+#         result.append(i)
+#         find_iteration_variables(n, result)
+#         result.pop()
+#
+#
+# find_iteration_variables(int(input()), [])
