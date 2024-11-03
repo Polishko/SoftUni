027d5f4f19a8ba1cc29e6bc302be62d92faf7bdf
Gizmo <nalanbilgin@gmail.com>
2024-11-03 11:08:57 +0200

    Added lecturers solution for Qu1.

--- Algorithms/Recursion/Exercises/1. Reverse Array.py
+++ Algorithms/Recursion/Exercises/1. Reverse Array.py
@@ -1,9 +1,20 @@
+# Lecturer`s solution
+def reverse_array(array, idx):
+    if idx == len(array) // 2:
+        print(' '.join(array))
+        return
 
-def reverse_array(array, n=0):
-    if n == len(array):
-         return
+    swap_idx = len(array) - idx - 1
+    array[idx], array[swap_idx] = array[swap_idx], array[idx]
+    reverse_array(array, idx + 1)
 
-    reverse_array(array, n + 1)
-    print(array[n], end=' ')
+reverse_array(input().split(), 0)
 
-reverse_array(input().split())
+# def reverse_array(array, n=0):
+#     if n == len(array):
+#          return
+#
+#     reverse_array(array, n + 1)
+#     print(array[n], end=' ')
+#
+# reverse_array(input().split())
