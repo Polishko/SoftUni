29c0afefde8eb8e8e76ec95b049385cde7ddaf48
Gizmo <nalanbilgin@gmail.com>
2024-11-04 16:04:21 +0200

    Added improvements to Qu5

--- Algorithms/Recursion/Exercises/5. Word Cruncher.py
+++ Algorithms/Recursion/Exercises/5. Word Cruncher.py
@@ -2,29 +2,32 @@ from collections import deque
 
 
 def word_cruncher(pieces, target, result, combinations):
+    # base cases
     if len(''.join(result)) > len(target):
         return
 
-    if ''.join(result) == target and ' '.join(result) not in combinations:
-        combinations.append(' '.join(result))
+    if ''.join(result) == target:
+        combinations.add(' '.join(result))
         return
 
     for _ in range(len(pieces)):
         piece = pieces.popleft()
         result.append(piece)
 
+        # continue searching if match
         if target.startswith(''.join(result)):
             word_cruncher(pieces, target, result, combinations)
 
+        # backtracking
         result.pop()
         pieces.append(piece)
 
 
 def rotate_words(pieces, target):
-    combinations = []
+    combinations = set()
     for _ in range(len(pieces)):
         pieces.rotate(-1)
-        word_cruncher(pieces, target, [], combinations)
+        word_cruncher(deque(pieces), target, [], combinations)
 
     for combination in combinations:
         print(''.join(combination))
