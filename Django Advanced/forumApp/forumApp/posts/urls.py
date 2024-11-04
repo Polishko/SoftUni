3456136e964e8c3913be68411e244374725dee59
Gizmo <nalanbilgin@gmail.com>
2024-11-04 12:12:07 +0200

    Feature: Added url for post approval

--- Django Advanced/forumApp/forumApp/posts/urls.py
+++ Django Advanced/forumApp/forumApp/posts/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 
 from forumApp.posts.views import IndexView, \
-    RedirectHomeView, DashboardView, AddPostView, EditPostView, DeletePostView, PostDetailView
+    RedirectHomeView, DashboardView, AddPostView, EditPostView, DeletePostView, PostDetailView, approve_post
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('add-post/', AddPostView.as_view(), name='add-post'),
     path('<int:pk>/', include([
+        path('approve', approve_post, name='approve'),
         path('details-post', PostDetailView.as_view(), name='details-post'),
         path('delete-post/', DeletePostView.as_view(), name='delete-post'),
         path('edit-post/', EditPostView.as_view(), name='edit-post'),
