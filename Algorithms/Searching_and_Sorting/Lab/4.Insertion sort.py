90dc1ec6a12f24d43d80a85e608c3daab54a1469
Gizmo <nalanbilgin@gmail.com>
2024-11-06 20:15:21 +0200

    Added lecturers improvement to solution of Qu4

--- Algorithms/Searching_and_Sorting/Lab/4.Insertion sort.py
+++ Algorithms/Searching_and_Sorting/Lab/4.Insertion sort.py
@@ -4,5 +4,7 @@ for j in range(1, len(arr)):
     for i in range(j, 0, -1):
         if arr[i] < arr[i - 1]:
             arr[i], arr[i - 1] = arr[i - 1], arr[i]
+        else:
+            break
 
 print(*arr, sep=' ')
