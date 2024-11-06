5af4224a95e741d593af606510c5209b03990765
Gizmo <nalanbilgin@gmail.com>
2024-11-06 18:29:23 +0200

    Addition: Added inline fields from profile to admin

--- Django Advanced/forumApp/forumApp/accounts/admin.py
+++ Django Advanced/forumApp/forumApp/accounts/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
+from django.contrib.admin import StackedInline
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from forumApp.accounts.forms import CustomUserForm, CustomUserChangeForm
+from forumApp.accounts.models import Profile
 
 UserModel = get_user_model()
 
+class ProfileInline(StackedInline):
+    model = Profile
+    can_delete = False
+    fields = ('age', 'points',)
+
 @admin.register(UserModel)
 class AppUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
     add_form = CustomUserForm
     form = CustomUserChangeForm
 
