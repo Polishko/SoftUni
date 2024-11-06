8bae5de81935130969495f643f625b327dec8798
Gizmo <nalanbilgin@gmail.com>
2024-11-06 12:28:42 +0200

    Added a new Profile model with one-to-one rel user with Custom User

--- Django Advanced/forumApp/forumApp/accounts/models.py
+++ Django Advanced/forumApp/forumApp/accounts/models.py
@@ -5,7 +5,29 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+
 class CustomUser(AbstractUser):
+    points = models.IntegerField(
+        default=0,
+    )
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        to=CustomUser,
+        on_delete=models.CASCADE,
+    )
+
+    age = models.IntegerField()
+
+    first_name = models.CharField(
+        max_length=30,
+    )
+
+    last_name = models.CharField(
+        max_length=30,
+    )
+
     points = models.IntegerField(
         default=0,
     )
