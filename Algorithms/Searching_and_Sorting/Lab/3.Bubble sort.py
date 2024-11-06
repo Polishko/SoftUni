cf2f9b374b0d2fa14d0f88ed609fbb3a0da47fd3
Gizmo <nalanbilgin@gmail.com>
2024-11-06 19:46:28 +0200

    Added lecturers solution for Qu3

--- Algorithms/Searching_and_Sorting/Lab/3.Bubble sort.py
+++ Algorithms/Searching_and_Sorting/Lab/3.Bubble sort.py
@@ -1,8 +1,24 @@
+# lecturer`s solution
 arr = [int(x) for x in input().split()]
 
-for idx in range(len(arr) - 1):
-    for next_idx in range(len(arr) - idx - 1):
-        if arr[next_idx + 1] < arr[next_idx]:
-            arr[next_idx], arr[next_idx + 1] = arr[next_idx + 1], arr[next_idx]
+is_sorted = False
+counter = 0
+
+while not is_sorted:
+    is_sorted = True
+    for idx in range(1, len(arr)):
+        if arr[idx] < arr[idx - 1]:
+            arr[idx - 1], arr[idx] = arr[idx], arr[idx - 1]
+            is_sorted = False
 
 print(*arr, sep=' ')
+
+
+# arr = [int(x) for x in input().split()]
+#
+# for idx in range(len(arr) - 1):
+#     for next_idx in range(len(arr) - idx - 1):
+#         if arr[next_idx + 1] < arr[next_idx]:
+#             arr[next_idx], arr[next_idx + 1] = arr[next_idx + 1], arr[next_idx]
+#
+# print(*arr, sep=' ')
