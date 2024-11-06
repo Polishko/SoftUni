acc9fb58cb447c7a5283df5b4baad23fe889f1ba
Gizmo <nalanbilgin@gmail.com>
2024-11-06 20:08:03 +0200

    Added solution for Qu4

---
+++ Algorithms/Searching_and_Sorting/Lab/4.Insertion sort.py
@@ -0,0 +1,8 @@
+arr = [int(x) for x in input().split()]
+
+for j in range(1, len(arr)):
+    for i in range(j, 0, -1):
+        if arr[i] < arr[i - 1]:
+            arr[i], arr[i - 1] = arr[i - 1], arr[i]
+
+print(*arr, sep=' ')
