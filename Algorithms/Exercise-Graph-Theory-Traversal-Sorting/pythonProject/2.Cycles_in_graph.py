5ba98d032aec5413dacdc8dca6518c2721512f0c
Gizmo <nalanbilgin@gmail.com>
2024-11-19 22:45:05 +0200

    Added solution to Qu2

--- Algorithms/Exercise-Graph-Theory-Traversal-Sorting/pythonProject/2.Cycles_in_graph.py
+++ Algorithms/Exercise-Graph-Theory-Traversal-Sorting/pythonProject/2.Cycles_in_graph.py
@@ -1,21 +1,63 @@
-graph = []
-starting = set()
-ending = set()
-is_acyclic = 'No'
+# lecturer`s solution with dfs
+def dfs(node, g, visited, current_cycle):
+    if node in current_cycle:
+        raise Exception
+    if node in visited:
+        return 
+    
+    visited.add(node)
+    current_cycle.add(node)
+
+    for child in g[node]:
+        dfs(child, g, visited, current_cycle)
+
+    current_cycle.remove(node)
+    
+g = {}
+visited = set()
 
 while True:
     line = input()
-
+    
     if line == 'End':
         break
+        
+    source, destination = line.split('-')
+    
+    if source not in g:
+        g[source] = []
+    if destination not in g:
+        g[destination] = []
+    g[source] = destination
+    
+try:
+    for node in g:
+        dfs(node, g, visited, set())
+    print('Acyclic: Yes')
+except Exception:
+    print('Acyclic: No')
+    
 
-    start, end = line.split('-')
 
-    starting.add(start)
-    ending.add(end)
-
-
-if len(starting - ending) != 0:
-    is_acyclic = 'Yes'
-
-print(f'Acyclic: {is_acyclic}')
+# initial solution
+# graph = []
+# starting = set()
+# ending = set()
+# is_acyclic = 'No'
+# 
+# while True:
+#     line = input()
+# 
+#     if line == 'End':
+#         break
+# 
+#     start, end = line.split('-')
+# 
+#     starting.add(start)
+#     ending.add(end)
+# 
+# 
+# if len(starting - ending) != 0:
+#     is_acyclic = 'Yes'
+# 
+# print(f'Acyclic: {is_acyclic}')
