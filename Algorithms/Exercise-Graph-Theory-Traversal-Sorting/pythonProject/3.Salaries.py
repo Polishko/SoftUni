427ff50ab3be50edc0ad33f509c43169c6cd726e
Gizmo <nalanbilgin@gmail.com>
2024-11-20 20:48:09 +0200

    Added lecturers solution for Qu3

--- Algorithms/Exercise-Graph-Theory-Traversal-Sorting/pythonProject/3.Salaries.py
+++ Algorithms/Exercise-Graph-Theory-Traversal-Sorting/pythonProject/3.Salaries.py
@@ -1,35 +1,76 @@
-def dfs(node, g, visited, node_salaries):
+# Lecturer`s optimized solution
+def dfs(node, graph, salaries):
+    # if node salary already set, return to get value
+    if salaries[node] is not None:
+        return salaries[node]
+    # else if node with no children, set salary 1 as per requirements
+    if len(graph[node]) == 0:
+        salaries[node] = 1
+        return 1 # then return its value
 
-    if node in visited:
-        return
+    # now apply dfs to traverse children
+    salary = 0 # salary from all children
+    for child in graph[node]:
+        salary += dfs(child, graph, salaries)
 
-    if len(g[node]) == 0:
-        node_salaries[node] +=1
-
-    visited.add(node)
-
-    for child in g[node]:
-        dfs(child, g, visited, node_salaries)
-        node_salaries[node] += node_salaries[child]
+    salaries[node] = salary # set node salary
+    return salary # return value
 
 
-num = int(input())
-g = {}
-node_salaries = {}
-for i in range(num):
+nodes = int(input())
+graph = []
 
-    if i not in g:
-        g[i] = []
-        node_salaries[i] = 0
+for i in range(nodes):
+    line = input()
+    children = []
+    for idx, state in enumerate(line):
+        if state == 'Y':
+            children.append(idx)
+    graph.append(children)
 
-    line = list(input())
-    for j in range(num):
-        if line[j] == 'Y':
-            g[i].append(j)
+salaries = [None] * nodes
 
-visited = set()
+result = 0
+for node in range(nodes):
+    salary_node = dfs(node, graph, salaries)
+    result += salary_node
 
-for node in g:
-    dfs(node, g, visited, node_salaries)
+print(result)
 
-print(sum(node_salaries.values()))
+
+# initial solution
+# def dfs(node, g, visited, node_salaries):
+#
+#     if node in visited:
+#         return
+#
+#     if len(g[node]) == 0:
+#         node_salaries[node] +=1
+#
+#     visited.add(node)
+#
+#     for child in g[node]:
+#         dfs(child, g, visited, node_salaries)
+#         node_salaries[node] += node_salaries[child]
+#
+#
+# num = int(input())
+# g = {}
+# node_salaries = {}
+# for i in range(num):
+#
+#     if i not in g:
+#         g[i] = []
+#         node_salaries[i] = 0
+#
+#     line = list(input())
+#     for j in range(num):
+#         if line[j] == 'Y':
+#             g[i].append(j)
+#
+# visited = set()
+#
+# for node in g:
+#     dfs(node, g, visited, node_salaries)
+#
+# print(sum(node_salaries.values()))
