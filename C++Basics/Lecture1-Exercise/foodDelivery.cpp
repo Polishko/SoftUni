3a8aa026ccaaed86b83ca0ab998fd30d42be1651
Gizmo <nalanbilgin@gmail.com>
2025-01-23 07:18:25 +0200

    Added a solution

---
+++ C++Basics/Lecture1-Exercise/foodDelivery.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+// int main() {
+//     double poultryMenu = 10.35;
+//     double fishMenu = 12.40;
+//     double vegetarianMenu = 8.15;
+//     double dessertCostPercent = 20;
+//     double deliveryCost = 2.50;
+
+//     int poultryOrderCount;
+//     int fishOrderCount;
+//     int vegetarianOrderCount;
+
+//     cin >> poultryOrderCount;
+//     cin >> fishOrderCount;
+//     cin >> vegetarianOrderCount;
+
+//     double totalWithoutDessert = poultryMenu * poultryOrderCount + fishMenu * fishOrderCount + vegetarianMenu * vegetarianOrderCount;
+//     double dessertCost = totalWithoutDessert * dessertCostPercent / 100;
+
+//     double totalCost = totalWithoutDessert + dessertCost + deliveryCost;
+
+//     cout << totalCost << endl;
+
+//     return 0;
+// }
