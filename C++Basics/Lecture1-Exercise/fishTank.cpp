720349f805ca6972397326d2fcbda38892012d85
Gizmo <nalanbilgin@gmail.com>
2025-01-23 07:45:31 +0200

    Added a solution

---
+++ C++Basics/Lecture1-Exercise/fishTank.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     int length;
+//     int width;
+//     int height;
+//     double occupiedPercent;
+//     double CMToDMConversionFactor = 0.001;
+
+//     cin >> length;
+//     cin >> width;
+//     cin >> height;
+//     cin >> occupiedPercent;
+
+//     double volume = length * width * height * CMToDMConversionFactor;
+//     double waterCapacity = volume * (100 - occupiedPercent) /100;
+
+//     cout << waterCapacity << endl;
+
+//     return 0;
+// }
