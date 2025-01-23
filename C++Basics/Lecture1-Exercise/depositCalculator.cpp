3b4376b433b58da70bad399a3b9261f506a1fe03
Gizmo <nalanbilgin@gmail.com>
2025-01-23 06:12:34 +0200

    Added a solution

--- C++Basics/Lecture1-Exercise/depositCalculator.cpp
+++ C++Basics/Lecture1-Exercise/depositCalculator.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    double deposit;
-    cin >> deposit;
-    int duration;
-    cin >> duration;
-    double percentPerYear;
-    cin >> percentPerYear;
+// int main() {
+//     double deposit;
+//     cin >> deposit;
+//     int duration;
+//     cin >> duration;
+//     double percentPerYear;
+//     cin >> percentPerYear;
 
-    double resultSum = deposit + duration * ((deposit * percentPerYear / 100) / 12);
+//     double resultSum = deposit + duration * ((deposit * percentPerYear / 100) / 12);
 
-    cout.setf(ios::fixed);
-    cout.precision(2);
-    cout << resultSum << endl;
+//     cout.setf(ios::fixed);
+//     cout.precision(2);
+//     cout << resultSum << endl;
 
-    return 0;
-}
+//     return 0;
+// }
---
+++ C++Basics/Lecture1-Exercise/vacationBookList.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+// int main() {
+//     int pages;
+//     cin >> pages;
+//     int pagesPerHour;
+//     cin >> pagesPerHour;
+//     int readDays;
+//     cin >> readDays;
+
+//     double daysNeeded = round(pages / pagesPerHour / readDays);
+//     cout << daysNeeded << endl;
+
+//     return 0;
+// }
