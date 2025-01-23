c28250b79833cb906a1eac98d3f622ab19b21290
Gizmo <nalanbilgin@gmail.com>
2025-01-23 07:35:14 +0200

    Added a solution

---
+++ C++Basics/Lecture1-Exercise/basketballEquipment.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     double snikersCostPercent = 60;
+//     double equiomentCostPercent = 80;
+//     double basketballCostCoeff = 0.25;
+//     double accessoriesCostCoeff = 0.2;
+
+//     int trainingCost;
+//     cin >> trainingCost;
+
+//     double snikersCost = snikersCostPercent * trainingCost / 100;
+//     double equipmentCost = equiomentCostPercent * snikersCost / 100;
+//     double basketballCost = basketballCostCoeff * equipmentCost;
+//     double accessoriesCost = accessoriesCostCoeff * basketballCost;
+
+//     double totalCost = trainingCost + snikersCost + equipmentCost + basketballCost + accessoriesCost;
+
+//     cout << totalCost << endl; 
+
+//     return 0;
+// }
