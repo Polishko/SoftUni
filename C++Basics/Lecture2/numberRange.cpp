9a284baa07ea74747d31335187853e31c68d1d66
Gizmo <nalanbilgin@gmail.com>
2025-01-25 22:06:43 +0200

    Added a solution

---
+++ C++Basics/Lecture2/numberRange.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     int num;
+//     cin >> num;
+
+//     if (num < 100) {
+//         cout << "Less than 100" << endl;
+//     } else if (num <= 200) {
+//         cout << "Between 100 and 200" << endl;
+//     } else {
+//         cout << "Greater than 200" << endl;
+//     }
+
+//     return 0;
+// }
