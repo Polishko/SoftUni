0328d29b35c67d0e4be0c02d55c941e938547333
Gizmo <nalanbilgin@gmail.com>
2025-01-25 22:12:46 +0200

    Added a solution

---
+++ C++Basics/Lecture2/speedInfo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     double speed;
+//     cin >> speed;
+
+//     string output;
+
+//     if (speed <= 10) {
+//         output = "slow";
+//     } else if (speed <= 50) {
+//         output = "average";
+//     } else if (speed <= 150) {
+//         output = "fast";
+//     } else if (speed <= 1000) {
+//         output = "ultra fast";
+//     } else {
+//         output = "extremely fast";
+//     }
+
+//     cout << output << endl;
+
+//     return 0;
+// }
