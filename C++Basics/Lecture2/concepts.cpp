5891179b7abe7c27d0dd02a2e97fb2b8c726c908
Gizmo <nalanbilgin@gmail.com>
2025-01-25 16:49:40 +0200

    Added a solution

--- C++Basics/Lecture2/concepts.cpp
+++ C++Basics/Lecture2/concepts.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int a = 1;
-    int b = 2;
-    cout << (a < b) << ' ' << (a > b) << endl;
-    return 0;
-}
+// int main() {
+//     int a = 1;
+//     int b = 2;
+//     cout << (a < b) << ' ' << (a > b) << endl;
+//     return 0;
+// }
