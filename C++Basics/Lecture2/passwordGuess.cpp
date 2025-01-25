7e863c114e0722d4670885a0bcee6bdbfb67efba
Gizmo <nalanbilgin@gmail.com>
2025-01-25 22:02:10 +0200

    Added a solution

---
+++ C++Basics/Lecture2/passwordGuess.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     string correctPassword = "s3cr3t!P@ssw0rd";
+
+//     string passwordInput;
+//     cin >> passwordInput;
+
+//     cout << (passwordInput == correctPassword ? "Welcome" : "Wrong password!") << endl;
+//     return 0;
+// }
