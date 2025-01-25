9490d65e4082564f107c905688cc8250646a8eba
Gizmo <nalanbilgin@gmail.com>
2025-01-25 18:12:14 +0200

    Added a solution

---
+++ C++Basics/Lecture2/evenOdd.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     int num;
+//     cin >> num;
+
+//     // alternative 3: Least significant bit (LSB) comparison
+
+//     cout << (num & 1 ? "odd" : "even") << endl;
+
+//     // // alternative 2
+
+//     // cout << (num % 2 == 0 ? "even" : "odd") << endl;
+
+//     // // alternative 1
+
+//     // if (num % 2 == 0) {
+//     //     cout << "even" << endl;
+//     // } 
+//     // else {
+//     //     cout << "odd" << endl;
+//     // }
+
+//     return 0;
+// }
