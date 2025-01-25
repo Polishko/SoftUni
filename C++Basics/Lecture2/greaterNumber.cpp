b214dcbdfac619992191bdf976ad307fcd4c6fdf
Gizmo <nalanbilgin@gmail.com>
2025-01-25 17:51:50 +0200

    Added a solution

---
+++ C++Basics/Lecture2/greaterNumber.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+// int main() {
+//     int a;
+//     int b;
+//     cin >> a;
+//     cin >> b;
+
+//     // alternative 3
+
+//     int greaterNum = a ^ ((a ^ b) & -(a < b));
+//     cout << greaterNum << endl;
+
+//     // alternative 2
+
+//     // if (a > b) {
+//     //     cout << a << endl;
+//     // } else {
+//     //     cout << b << endl;
+//     // }
+
+//     // alternative 1
+
+//     // cout << max(a, b) << endl;
+//     return 0;
+// }
